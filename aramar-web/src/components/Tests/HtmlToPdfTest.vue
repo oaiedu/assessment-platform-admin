a491e42227ebcb752c1aa1a92f2ea38df486153d
octaviohmb <octaviohmb@gmail.com>
2020-01-31 17:38:10 -0300

    tables

--- aramar-web/src/components/Tests/HtmlToPdfTest.vue
+++ aramar-web/src/components/Tests/HtmlToPdfTest.vue
@@ -6,135 +6,154 @@
 </template>
 
 <script>
-import jsPDF from 'jspdf'
-import html2canvas from 'html2canvas'
-import fontFile from '@/assets/ARIALUNI.TTF'
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import fontFile from "@/assets/ARIALUNI.TTF";
 
 export default {
-    props: ["test"],
-    data () {
-        return {
-            letters: ['A','B','C','D'],
-        }
-    },
-    computed: {
-        currentTest() {
-            let aux = []
+  props: ["test"],
+  data() {
+    return {
+      letters: ["A", "B", "C", "D"]
+    };
+  },
+  computed: {
+    currentTest() {
+      let aux = [];
 
-            this.test.data.PERGUNTAS.forEach(element =>
-                aux.push(this.$store.getters.findQuestionById(element))
-            )
+      this.test.data.PERGUNTAS.forEach(element =>
+        aux.push(this.$store.getters.findQuestionById(element))
+      );
 
-            return aux
-        }
-    },
-    methods: {
-      generatePdf(){
-
-        var pageWidth = 8,
-            lineHeight = 1.2,
-            margin = 1,
-            maxLineWidth = pageWidth - margin * 2,
-            fontSize = 12
-
-        var doc = new jsPDF({
-          unit: 'in'
-        })
-
-        var fontInBase64 = '',
-            fileName = '',
-            reader = new FileReader()
-
-        //     console.log("fonte: ", fontFile)
-        // // var base64 = require('file-base64');
-        //
-        // // base64.encode('@/assets/ARIALUNI.TTF', function(err, base64String) {
-        // //   console.log("xixizinho")
-        // //   console.log(base64String);
-        // // });
-        // // fontInBase64 = reader.result.split(',')[1];
-        // // fileName = fontFile.name.replace(/\s+/g, '-');
-        // // fileNameWithoutExtension = fileName.split('.')[0],
-        // doc.addFileToVFS("Arial Unicode MS", fontFile);
-        // doc.addFont("Arial Unicode MS");
-        //
-        // doc.setFont("Arial Unicode MS");
-        doc.setFontSize(fontSize)
-
-        this.currentTest.forEach( element => {
-          if ( typeof element.data.IMAGENS === 'undefined' || element.data.IMAGENS === "" ){
-            let texttohtml = `
-ASSUNTO: ${element.data.DISCIPLINA}
-CONHECIMENTO: ${element.data.CONHECIMENTO} [${element.data.RELEVANCIA_OR}/${element.data.RELEVANCIA_OSR}]
-IQ: ${element.id}
-
-        ${element.data.PERGUNTA}
-
-A - ${element.data.RESPOSTAS[0].text}
-
-B - ${element.data.RESPOSTAS[1].text}
-
-C - ${element.data.RESPOSTAS[2].text}
-1909
-D - ${element.data.RESPOSTAS[3].text}
-            `
-            let textLines = doc.splitTextToSize(texttohtml,maxLineWidth)
-            doc.text(textLines, margin, margin)
-
-            doc.addPage( "a4", "p" )
-          }
-
-          else {
-            let texttohtml = `
-ASSUNTO: ${element.data.DISCIPLINA}
-CONHECIMENTO: ${element.data.CONHECIMENTO} [${element.data.RELEVANCIA_OR}/${element.data.RELEVANCIA_OSR}]
-IQ: ${element.id}
-
-        ${element.data.PERGUNTA}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-A - ${element.data.RESPOSTAS[0].text}
-
-B - ${element.data.RESPOSTAS[1].text}
-
-C - ${element.data.RESPOSTAS[2].text}
-
-D - ${element.data.RESPOSTAS[3].text}
-            `
-            let textLines = doc.splitTextToSize(texttohtml,maxLineWidth)
-            doc.text(textLines, margin, margin)
-
-            doc.addImage(element.data.IMAGENS, "JPEG", pageWidth/4, 3.5, 3, 3)
-
-            doc.addPage( "a4", "p" )
-          }
-        })
-
-        doc.save("test.pdf")
-      }
+      return aux;
     }
-}
+  },
+  methods: {
+    generatePdf() {
+      var pageWidth = 203.2,
+        lineHeight = 30.48,
+        margin = 25.4,
+        maxLineWidth = pageWidth - margin * 2,
+        fontSize = 12;
+
+      var doc = new jsPDF()
+
+      doc.setFontSize(fontSize)
+
+      var fontInBase64 = "",
+        fileName = "",
+        reader = new FileReader();
+
+      this.currentTest.forEach( element => {
+        if ( typeof element.data.IMAGENS === 'undefined' || element.data.IMAGENS === "" ){
+          let texttohtml = `
+ASSUNTO: ${element.data.DISCIPLINA}
+CONHECIMENTO: ${element.data.CONHECIMENTO} [${element.data.RELEVANCIA_OR}/${element.data.RELEVANCIA_OSR}]
+IQ: ${element.id}
+
+      ${element.data.PERGUNTA}
+
+A - ${element.data.RESPOSTAS[0].text}
+
+B - ${element.data.RESPOSTAS[1].text}
+
+C - ${element.data.RESPOSTAS[2].text}
+
+D - ${element.data.RESPOSTAS[3].text}
+          `
+          let textLines = doc.splitTextToSize(texttohtml,maxLineWidth)
+          doc.text(textLines, margin, margin)
+
+          doc.addPage( "a4", "p" )
+        }
+
+        else {
+          let texttohtml = `
+ASSUNTO: ${element.data.DISCIPLINA}
+CONHECIMENTO: ${element.data.CONHECIMENTO} [${element.data.RELEVANCIA_OR}/${element.data.RELEVANCIA_OSR}]
+IQ: ${element.id}
+
+      ${element.data.PERGUNTA}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+A - ${element.data.RESPOSTAS[0].text}
+
+B - ${element.data.RESPOSTAS[1].text}
+
+C - ${element.data.RESPOSTAS[2].text}
+
+D - ${element.data.RESPOSTAS[3].text}
+          `
+          let textLines = doc.splitTextToSize(texttohtml,maxLineWidth)
+          doc.text(textLines, margin, margin)
+
+          doc.addImage(element.data.IMAGENS, "JPEG", pageWidth/4, 100, 76.2, 76.2)
+
+          doc.addPage( "a4", "p" )
+        }
+      })
+
+    var letters = ["A", "B", "C", "D"];
+
+    var generateData = (amount) => {
+      var result = []
+      var data = {
+        QUESTÃO: "",
+        RESPOSTA: ""
+      }
+
+      for(var i = 0; i < amount; i++){
+        data.QUESTÃO = `${i+1}`
+
+        for(var j = 0; j < 4; j++ ){
+          if ( this.currentTest[i].data.RESPOSTAS[j].value === true ) {
+            data.RESPOSTA = letters[j]
+          }
+        }
+        result.push(Object.assign({}, data))
+      }
+      return result
+    };
+
+    function createHeaders(keys) {
+      var result = [];
+      for (var i = 0; i < keys.length; i += 1) {
+        result.push({
+          name: keys[i],
+          prompt: keys[i],
+          width: 40
+        });
+      }
+      return result;
+    }
+
+      var headers = createHeaders(["QUESTÃO", "RESPOSTA"]);
+      doc.table(pageWidth/4, margin, generateData(this.currentTest.length), headers);
+      doc.save("test.pdf")
+    }
+  }
+};
 </script>
