b579f63eb3a217676b152851f3b2cf67c8eba7c7
Michell Algarra <michell.algarra@gmail.com>
2020-10-09 16:14:09 -0300

    Fixed Questions code pattern after it's replace in merge

--- src/store/Questions.js
+++ src/store/Questions.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase'
+import { db, storage } from '../main';
 
 export default {
     state: {
@@ -21,195 +21,191 @@ export default {
     },
     mutations: {
         setLoadedQuestions(state, payload) {
-            state.loadedQuestions = payload
+            state.loadedQuestions = payload;
         },
         setDeletedQuestion(state, payload) {
-            state.deleteQuestionId = payload
+            state.deleteQuestionId = payload;
         },
         createQuestion(state, payload) {
-            state.loadedQuestions.push(payload)
+            state.loadedQuestions.push(payload);
         },
         deleteQuestion(state, payload) {
             state.loadedQuestions.forEach((item, i) => {
-              if(item.id === payload)
-                state.loadedQuestions.splice(i, 1);
+                if(item.id === payload)
+                    state.loadedQuestions.splice(i, 1);
             });
         }
     },
     actions: {
         loadedQuestions({ commit }) {
-            commit('setLoading', true)
-            const db = firebase.firestore().collection("questions")
-            let questions = []
-            db.get().then(querySnapshot => {
+            commit('setLoading', true);
+            let questions = [];
+            db.collection('questions').get().then(querySnapshot => {
                 querySnapshot.forEach(doc => {
-                    questions.push(Object.assign({ id: doc.id, data: doc.data() }))
+                    questions.push(Object.assign({ id: doc.id, data: doc.data() }));
                 })
-                commit('setLoadedQuestions', questions)
-                commit('setLoading', false)
+                commit('setLoadedQuestions', questions);
+                commit('setLoading', false);
             })
         },
         deleteQuestion({ commit, dispatch }, payload) {
-            commit('setLoading', true)
-            const db = firebase.firestore()
-            const id = payload
+            commit('setLoading', true);
+            const id = payload;
             return db.collection("questions").doc(id).delete()
                 .then( () => {
-                    commit('setLoading', false)
-                    commit('deleteQuestion', id)
-                    console.log("Document successfully deleted!")
+                    commit('setLoading', false);
+                    commit('deleteQuestion', id);
+                    console.log("Document successfully deleted!");
                 })
                 .catch(function (error) {
                     console.error("Error removing document: ", error);
                 });
         },
         uploadImage({ commit }, payload) {
-          const puta = new Promise((resolve,reject)=>{
-            try{
-              const storageRef = firebase.storage().ref()
-              const file = payload.images
-              var images = storageRef.child(file.name).put(file)
-                .then(function(snapshot){
-                  console.log("Uploaded a file!: ", snapshot)
-                  snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                    console.log('File available at', downloadURL);
-                    resolve(downloadURL.toString())
-                  });
-                })
-                .catch(function(error){
-                  console.error("Error uploading file",error)
-
-                  resolve(error)
-                })
-            }catch{
-              reject("yuuiijioj")
-            }
-          })
-
-          return puta
+            const puta = new Promise((resolve,reject) => {
+                try {
+                    const storageRef = storage.ref()
+                    const file = payload.images;
+                    const images = storageRef.child(file.name).put(file)
+                        .then(snapshot => {
+                            console.log("Uploaded a file!: ", snapshot)
+                            snapshot.ref.getDownloadURL().then(downloadURL => {
+                                console.log('File available at', downloadURL);
+                                resolve(downloadURL.toString());
+                            });
+                        })
+                        .catch(error => {
+                            console.error("Error uploading file",error);
+                            resolve(error);
+                        });
+                } catch{
+                    reject("yuuiijioj");
+                }
+            });
+            return puta;
         },
         editQuestion({ commit, dispatch }, payload) {
-          commit('setLoading', true)
+            commit('setLoading', true);
 
-          var today = new Date();
-
-          const db = firebase.firestore()
-          const question = {
-              id: payload.questionData.id,
-              questionDescription: payload.questionData.questionDescription,
-              subject: payload.questionData.subject,
-              knowledge: payload.questionData.knowledge,
-              knowledgePWR: payload.questionData.knowledgePWR,
-              knowledgeBWR: payload.questionData.knowledgeBWR,
-              answers: payload.questionData.answers,
-              images: payload.questionData.images,
-              imageSize: payload.questionData.imageSize,
-          }
-          const edition = payload.oldData.edited
-          edition.push({
-            id: payload.user,
-            date: today,
-            question: payload.oldData.id + "-" + (payload.oldData.edited.length + 1)
-          })
-
-          db.collection("questions").doc(question.id).update({
-              PERGUNTA: question.questionDescription,
-              DISCIPLINA: question.subject,
-              CONHECIMENTO: question.knowledge,
-              RELEVANCIA_OR: question.knowledgePWR,
-              RELEVANCIA_OSR: question.knowledgeBWR,
-              RESPOSTAS: question.answers,
-              IMAGENS: question.images,
-              TAMANHO_IMAGEM: question.imageSize,
-              edited: edition
-          })
-              .then( () => {
-                  dispatch("createdEditedQuestion", payload.oldData)
-                  console.log("Success edit")
-              })
-              .catch(function (error) {
-                  console.error("Error writing document: ", error);
-              });
-        },
-        createdEditedQuestion({ commit, dispatch }, payload) {
-          const db = firebase.firestore()
-          const editedQuestion = {
-              id: payload.id,
-              questionDescription: payload.questionDescription,
-              subject: payload.subject,
-              knowledge: payload.knowledge,
-              knowledgePWR: payload.knowledgePWR,
-              knowledgeBWR: payload.knowledgeBWR,
-              answers: payload.answers,
-              images: payload.images,
-              imageSize: payload.imageSize
-          }
-          db.collection("edited questions").doc(editedQuestion.id + "-" + payload.edited.length).set({
-              PERGUNTA: editedQuestion.questionDescription,
-              DISCIPLINA: editedQuestion.subject,
-              CONHECIMENTO: editedQuestion.knowledge,
-              RELEVANCIA_OR: editedQuestion.knowledgePWR,
-              RELEVANCIA_OSR: editedQuestion.knowledgeBWR,
-              RESPOSTAS: editedQuestion.answers,
-              IMAGENS: editedQuestion.images,
-              TAMANHO_IMAGEM: editedQuestion.imageSize,
-          })
-              .then( () => {
-                  commit('setLoading', false)
-                  console.log("Success create edit")
-              })
-              .catch(function (error) {
-                  console.error("Error writing document: ", error);
-              });
-        },
-        createQuestion({ commit, dispatch }, payload) {
-            const db = firebase.firestore()
-            var question = {
-              id: payload.id,
-              data: {
-                PERGUNTA: payload.questionDescription,
-                DISCIPLINA: payload.subject,
-                CONHECIMENTO: payload.knowledge,
-                RELEVANCIA_OR: payload.knowledgePWR,
-                RELEVANCIA_OSR: payload.knowledgeBWR,
-                RESPOSTAS: payload.answers,
-                IMAGENS: payload.images,
-                TAMANHO_IMAGEM: payload.imageSize,
-                edited: []
-              }
+            const question = {
+                id: payload.questionData.id,
+                questionDescription: payload.questionData.questionDescription,
+                subject: payload.questionData.subject,
+                knowledge: payload.questionData.knowledge,
+                knowledgePWR: payload.questionData.knowledgePWR,
+                knowledgeBWR: payload.questionData.knowledgeBWR,
+                answers: payload.questionData.answers,
+                images: payload.questionData.images,
+                imageSize: payload.questionData.imageSize,
             }
-            db.collection("questions").doc(question.id).set(question.data)
-                .then(function () {
-                    commit('setLoading', false)
-                    commit('createQuestion', question)
-                    console.log("Success")
+
+            const today = new Date();
+            const edition = payload.oldData.edited;
+            edition.push({
+                id: payload.user,
+                date: today,
+                question: payload.oldData.id + "-" + (payload.oldData.edited.length + 1)
+            });
+
+            db.collection("questions").doc(question.id)
+                .update({
+                    PERGUNTA: question.questionDescription,
+                    DISCIPLINA: question.subject,
+                    CONHECIMENTO: question.knowledge,
+                    RELEVANCIA_OR: question.knowledgePWR,
+                    RELEVANCIA_OSR: question.knowledgeBWR,
+                    RESPOSTAS: question.answers,
+                    IMAGENS: question.images,
+                    TAMANHO_IMAGEM: question.imageSize,
+                    edited: edition
                 })
-                .catch(function (error) {
+                .then(() => {
+                    dispatch("createdEditedQuestion", payload.oldData);
+                    console.log("Success edit");
+                })
+                .catch(error => {
+                    console.error("Error writing document: ", error);
+                });
+        },
+        createdEditedQuestion({ commit }, payload) {
+            const editedQuestion = {
+                id: payload.id,
+                questionDescription: payload.questionDescription,
+                subject: payload.subject,
+                knowledge: payload.knowledge,
+                knowledgePWR: payload.knowledgePWR,
+                knowledgeBWR: payload.knowledgeBWR,
+                answers: payload.answers,
+                images: payload.images,
+                imageSize: payload.imageSize
+            }
+            db.collection("edited questions").doc(editedQuestion.id + "-" + payload.edited.length)
+                .set({
+                    PERGUNTA: editedQuestion.questionDescription,
+                    DISCIPLINA: editedQuestion.subject,
+                    CONHECIMENTO: editedQuestion.knowledge,
+                    RELEVANCIA_OR: editedQuestion.knowledgePWR,
+                    RELEVANCIA_OSR: editedQuestion.knowledgeBWR,
+                    RESPOSTAS: editedQuestion.answers,
+                    IMAGENS: editedQuestion.images,
+                    TAMANHO_IMAGEM: editedQuestion.imageSize,
+                })
+                .then(() => {
+                    commit('setLoading', false);
+                    console.log("Success create edit");
+                })
+                .catch(error => {
+                    console.error("Error writing document: ", error);
+                });
+        },
+        createQuestion({ commit }, payload) {
+            const question = {
+                id: payload.id,
+                data: {
+                    PERGUNTA: payload.questionDescription,
+                    DISCIPLINA: payload.subject,
+                    CONHECIMENTO: payload.knowledge,
+                    RELEVANCIA_OR: payload.knowledgePWR,
+                    RELEVANCIA_OSR: payload.knowledgeBWR,
+                    RESPOSTAS: payload.answers,
+                    IMAGENS: payload.images,
+                    TAMANHO_IMAGEM: payload.imageSize,
+                    edited: []
+                }
+            }
+
+            db.collection("questions").doc(question.id).set(question.data)
+                .then(() => {
+                    commit('setLoading', false);
+                    commit('createQuestion', question);
+                    console.log("Success");
+                })
+                .catch(error => {
                     console.error("Error writing document: ", error);
                 });
         },
     },
     getters: {
-        getSubjects(state){
-          return state.subjects
+        getSubjects(state) {
+            return state.subjects;
         },
-        numberOfQuestions(state){
-          return state.loadedQuestions.length
+        numberOfQuestions(state) {
+            return state.loadedQuestions.length;
         },
         loadedQuestions(state) {
-            return state.loadedQuestions
+            return state.loadedQuestions;
         },
         getAnswersById(state) {
-          let aux = state.loadedQuestions.find(question => question.id === id)
-          return Object.assign({},aux.data.RESPOSTAS)
+            let aux = state.loadedQuestions.find(question => question.id === id);
+            return Object.assign({}, aux.data.RESPOSTAS);
         },
-        getNumberOfQuestionBySubject:(state) => (subject)=> {
-            let counter = 0
+        getNumberOfQuestionBySubject: state => subject => {
+            let counter = 0;
             state.loadedQuestions.forEach(element => {
                 if (element.data.DISCIPLINA === subject)
-                    counter++
-            })
-            return counter
+                    counter++;
+            });
+            return counter;
         },
         findQuestionById: state => id => state.loadedQuestions.find(question => question.id === id)
     }
