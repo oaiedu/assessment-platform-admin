cfa4b85afbe2a791f57d80e6ffb4808a99b7ac98
Michell Algarra <michell.algarra@gmail.com>
2020-10-27 19:41:29 -0300

    feat(store/paper): Implemented a method to upload images from documents and added a id to the payload when creating a new one

--- src/store/modules/paper.store.js
+++ src/store/modules/paper.store.js
@@ -1,4 +1,4 @@
-import { db } from '../../main';
+import { db, storage } from '../../main';
 
 const initialState = () => ({
     loadedPapers: [],
@@ -26,10 +26,46 @@ const mutations = {
 }
 
 const actions = {
+    uploadImagePaper({ commit }, payload) {
+        const request = new Promise((resolve,reject) => {
+            try {
+                const storageRef = storage.ref();
+                const file = payload.images;
+                const paperId = payload.id;
+                const format = `documents/document-$${paperId}`;
+                storageRef.child(format).put(file)
+                    .then(snapshot => {
+                        console.log("Uploaded a file!: ", snapshot)
+                        snapshot.ref.getDownloadURL().then(downloadURL => {
+                            console.log('File available at', downloadURL);
+                            resolve(downloadURL.toString());
+                        });
+                    })
+                    .catch(error => {
+                        console.error("Error uploading file", error);
+                        resolve(error);
+                    });
+            } catch{
+                reject();
+            }
+        });
+        return request;
+    },
     deletePaper({ commit, dispatch }, payload) {
         commit('setLoading', true);
         const id = payload;
-        return db.collection("papers").doc(id).delete()
+        return db.collection("papers").where('id', '==', id).get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    doc.ref.delete();
+                    if(doc.data().image && doc.data().image.length > 0) {
+                        const image = doc.data().image;
+                        const childImage = image.split('?alt=media')[0].split('/o/')[1];
+                        const child = decodeURIComponent(childImage);
+                        storage.ref().child(child).delete();
+                    }
+                });
+            })
             .then(() => {
                 commit('setLoading', false);
                 dispatch("loadedPapers");
@@ -45,7 +81,7 @@ const actions = {
         db.collection("papers").get()
             .then(querySnapshot => {
                 querySnapshot.forEach(doc => {
-                    papers.push(Object.assign({id: doc.id, data: doc.data()}));
+                    papers.push(Object.assign({id: doc.data().id, data: doc.data()}));
             });
             commit('setLoadedPapers', papers);
             commit('setLoading', false);
@@ -53,41 +89,38 @@ const actions = {
     },
     createPaper({ commit, dispatch }, payload) {
         const paper = {
+            id: payload.paperId,
             name: payload.paperName,
             image: payload.paperImage,
             description: payload.paperDescription
         }
-        db.collection("papers").doc().set({
-            name: paper.name,
-            image: paper.image,
-            description: paper.description
-        })
-        .then(() => {
-            commit('setLoading', false);
-            dispatch("loadedPapers");
-            console.log("Sucess");
-        })
-        .catch(error => {
-            console.error("Error writing document: ", error);
-        });
+        db.collection("papers").add(paper)
+            .then(() => {
+                commit('setLoading', false);
+                dispatch("loadedPapers");
+                console.log("Success");
+            })
+            .catch(error => {
+                console.error("Error writing document: ", error);
+            });
     },
     updatePaper({ commit, dispatch }, payload) {
         const paper = {
             name: payload.paperName,
             image: payload.paperImage,
             description: payload.paperDescription,
-            id: payload.id
+            id: payload.paperId
         }
-        db.collection("papers").doc(paper.id)
-            .update({
-                name: paper.name,
-                image: paper.image,
-                description: paper.description
+        db.collection("papers").where('id', '==', paper.id).get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    doc.ref.update(paper);
+                });
             })
             .then(() => {
                 commit('setLoading', false);
                 dispatch("loadedPapers");
-                console.log("Sucess");
+                console.log("Success");
             })
             .catch(error => {
                 console.error("Error writing document: ", error);
