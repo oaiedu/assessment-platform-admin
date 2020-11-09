c097fc21bc2517ac4c14bc55a1102673b81446a7
Michell Algarra <michell.algarra@gmail.com>
2020-11-09 17:33:17 -0300

    feat(firebase-functions/moveImages): Implemented a function to move images at storage to place them into their correct directories

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,39 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
-const storage = admin.storage();
+const storage = admin.storage().bucket();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.moveImages = functions.https.onRequest(async (req, res) => {
+    const data = [];
+    await db.collection('questions').get()
+        .then(snapshot => {
+            snapshot.forEach(async doc => {
+                const imageURL = doc.data().IMAGENS;
+                const image = decodeURIComponent(imageURL.split('?alt=media')[0].split('/o/')[1]);
+                const format = `questions/question-${doc.data().IQ}`;
+                if(image && image !== format) {
+                    storage.file(image).move(format);
+                    await storage.file(format).get().then(files => {
+                        files.forEach(file => {
+                            const link = file.selfLink;
+                            const token = file.metadata.firebaseStorageDownloadTokens;
+                            if(link && token) {
+                                const middle = '?alt=media&token=';
+                                const newLink = link.replace('www.googleapis.com/storage/v1', 'firebasestorage.googleapis.com/v0')
+                                    + middle + token;
+                                db.collection('questions').doc(doc.id).set({...doc.data(), IMAGENS: newLink});
+                                console.log(`From: '${image}' to '${format}', available at: ${newLink}\n`);
+                            }
+                        });
+                    });
+                    data.push({...doc.data(), IMAGENS: newLink});
+                } else {
+                    data.push(doc.data());
+                }
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    res.send(data);
+});
