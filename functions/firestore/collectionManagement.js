f8fef117c1559a4481d2d01edba3706c44a3a40f
Michell Algarra <michell.algarra@gmail.com>
2020-11-27 16:51:55 -0300

    feat(functions/collectionManagement): Created ways to get the IQ's back if needed, also fixed how firestore is imported back

--- functions/firestore/collectionManagement.js
+++ functions/firestore/collectionManagement.js
@@ -98,7 +98,7 @@ exports.deleteNonexistentReferences = async (req, res) => {
 
 exports.importFirestore = async (req, res) => {
     const folderRootName = 'backups';
-    const timestamp = '2020-11-26T17-32-39.212Z';
+    const timestamp = '2020-11-27T16-43-04.696Z';
     const path = `../../${folderRootName}/${timestamp}/firestore`;
     const collections = ['users', 'questions', 'question requests', 'edited questions', 'tests', 'papers'];
 
@@ -106,11 +106,16 @@ exports.importFirestore = async (req, res) => {
         const jsonPath = `${path}/${cn}-${timestamp}.json`;
         const file = fs.readFileSync(jsonPath);
         const json = JSON.parse(file);
+        const data = [];
+
+        for (const key in json) {
+            data.push([key, json[key]]);
+        }
 
         console.log(cn + ': ' + json.length);
 
-        json.forEach(doc => {
-            db.collection(cn).add(doc).catch(console.log);
+        data.forEach(doc => {
+            db.collection(cn).doc(doc[0]).set(doc[1]).catch(error => console.log(error));
         });
     });
 
@@ -119,32 +124,21 @@ exports.importFirestore = async (req, res) => {
 
 exports.importQuestions = async (req, res) => {
     const folderRootName = 'backups';
-    const timestamp = '2020-11-26T17-32-39.212Z';
+    const timestamp = '2020-11-27T16-43-04.696Z';
     const path = `../../${folderRootName}/${timestamp}/firestore`;
 
-    const jsonPath = `${path}/questions-${timestamp}.json`;
+    const jsonPath = `${path}/prod-questions-${timestamp}.json`;
     const file = fs.readFileSync(jsonPath);
     const json = JSON.parse(file);
+    const data = [];
 
-    // console.log(cn + ': ' + json.length);
+    for (const key in json) {
+        data.push([key, json[key]]);
+    }
 
-    // await db.collection('questions').get()
-    //     .then(snapshot => {
-    //         console.log('Len: ' + snapshot.docs.length);
-    //     }).catch(console.log);
-    // json.forEach(doc => {
-    //     if(doc.IMAGENS && doc.IMAGENS.length > 0) {
-    //         const imageName = decodeURIComponent(doc.IMAGENS.split('?alt=media')[0].split('/o/')[1]);
-    //         if(imageName.includes('vaso')) {
-    //             // console.log(imageName.split('_')[0]);
-    //             console.log(imageName.split(' ')[1].split('.')[0].replace('p', 'P'));
-    //             db.collection('questions').doc(imageName.split(' ')[1].split('.')[0].replace('p', 'P')).set(doc).catch(console.log);
+    db.collection('questions').get().then(snapshot => console.log(snapshot.docs.length)).catch(console.log);
 
-    //         }
-    //     }
-    // });
-
-    res.send('Firestore data imported!');
+    res.send('Firestore questions imported!');
 }
 
 exports.compareQuestions = async (req, res) => {
@@ -174,60 +168,159 @@ exports.compareQuestions = async (req, res) => {
         })
         .catch(console.log);
 
-    const promises = subjects.map(async sbj => {
-        return await db.collection('realtime-questions').where('DISCIPLINA', '==', sbj).get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                    const iq = doc.id;
-                    const klg = doc.data().CONHECIMENTO;
 
-                    auxQuestions.forEach(question => {
-                        if(question.CONHECIMENTO === klg
-                            && question.DISCIPLINA === sbj
-                            && !iqs.includes(iq)) {
-                            iqs.push(iq);
-                            updatedQuestions.push({ ...question, IQ: iq });
-                        }
-                    });
-                });
-            })
-            .catch(console.log);
-        });
 
-        await Promise.all(promises);
 
-        console.log(updatedQuestions.length);
-        updatedQuestions.forEach(doc => {
-            db.collection('updated-questions').add(doc).catch(console.log);
-        });
+    await Promise.all(promises);
+
+    console.log(updatedQuestions.length);
+    // updatedQuestions.forEach(doc => {
+    //     recDB.collection('updated-questions').doc(doc.IQ).set(doc).catch(console.log);
+    // });
 
     res.send('Done!');
 }
 
 exports.auxQuestions = async (req, res) => {
-    const folderRootName = 'backups';
-    const timestamp = '2020-11-26T17-32-39.212Z';
-    const path = `../../${folderRootName}/${timestamp}/firestore`;
+    const folderRootName = 'rdb-backups';
+    const timestamp = '2020-11-27T16-43-04.696Z';
+    const path = `../../${folderRootName}/${timestamp}`;
 
-    const jsonPath = `${path}/questions-${timestamp}.json`;
-    const file = fs.readFileSync(jsonPath);
-    const json = JSON.parse(file);
+    // const jsonPath = `${path}/rdb-${timestamp}.json`;
+    // const file = fs.readFileSync(jsonPath);
+    // const json = JSON.parse(file);
+    // const dataRDB = [];
 
+    const fileBkp = fs.readFileSync('../../backups/2020-11-27T16-43-04.696Z/firestore/questions-2020-11-27T16-43-04.696Z.json');
+    const jsonBkp = JSON.parse(fileBkp);
+    const dataBkp = [];
 
-    // json.forEach(doc => {
-    //     db.collection('aux-questions').add(doc).catch(console.log);
+    const prodTimestamp = '2020-11-25T16-23-01.553Z';
+    const prodPath = `../../backups/${prodTimestamp}/firestore-prod/prod-questions.json`;
+    const prodData = [];
+
+    const fileProd = fs.readFileSync(prodPath);
+    const jsonProd = JSON.parse(fileProd);
+
+    // for (const key in jsonBkp) {
+    //     // dataBkp.push([key, jsonBkp[key]]);
+    // }
+
+    // console.log('Prod length: ' + prodData.length);
+
+    // prodData.forEach(doc => {
+    //     db.collection('questions').doc(doc[0]).set(doc[1]).catch(error => console.log(error));
+    // });
+
+    const p3633 = {
+        CONHECIMENTO: 'C1.12',
+        DISCIPLINA: 'Transferência de Calor',
+        IMAGENS: '',
+        IQ: 'P3633',
+        PERGUNTA: 'O conjunto turbina + gerador principal opera com 80% de carga. O trocador de calor do sistema de óleo lubrificante da turbina principal apresentava as seguintes temperaturas iniciais em regime permanente: Temperatura do óleo lubrificante na entrada do trocador (To-e) = 79ºC Temperatura do óleo lubrificante na saída do trocador (To-s) = 45ºC Temperatura da água de resfriamento na entrada do trocador (Ta-e) = 29ºC Temperatura da água de resfriamento na saída do trocador (Ta-s) = 46ºC Passaram-se seis meses de operação. O trocador de calor do sistema de óleo lubrificante da turbina principal apresenta agora as seguintes temperaturas <u>atuais</u> em regime permanente: To-e = 82ºC To-s = 48ºC Ta-e = 29ºC Ta-s = 46ºC Presuma que ao longo dos seis meses de operação as vazões e os calores específicos da água de resfriamento e do óleo lubrificante permaneceram inalteradas. Qual das seguintes situações pode ter sido responsável pela diferença observada no trocador de calor entre as temperaturas em regime permanente iniciais e atuais?',
+        RELEVANCIA_OR: '2.5',
+        RELEVANCIA_OSR: '2.7',
+        RESPOSTAS: [
+            {
+                ansId: 'radio-1',
+                text: 'Houve crescimento de incrustação nos tubos do trocador de calor.',
+                value: true
+            },
+            {
+                ansId: 'radio-2',
+                text: 'A temperatura da fonte de água de resfriamento aumentou.',
+                value: false
+            },
+            {
+                ansId: 'radio-3',
+                text: 'Aumentou a carga sobre o conjunto turbina + gerador principal.',
+                value: false
+            },
+            {
+                ansId: 'radio-4',
+                text: 'Diminuiu a carga sobre o conjunto turbina + gerador principal.',
+                value: false
+            }
+        ],
+        edited: []
+    }
+
+    jsonBkp['P3633'] = p3633;
+
+    const prodBkp = JSON.stringify(jsonBkp);
+
+    fs.writeFileSync(`../../backups/2020-11-27T16-43-04.696Z/firestore/prod-questions-${timestamp}.json`, prodBkp);
+
+    // for (const key in json) {
+    //     dataRDB.push([key, json[key]]);
+    // }
+
+    // prodData.push(...Object.values(jsonProd));
+
+    // const updated = prodData.map(doc => doc.PERGUNTA);
+
+    // Promise.all(updated);
+
+    // const data = [];
+    // dataRDB.forEach(doc => {
+    //     if(!updated.includes(doc[1].PERGUNTA)) {
+    //         data.push(doc);
+    //         console.log(doc[0]);
+    //     }
+    // });
+    // console.log(data.length);
+
+    // data.forEach(doc => {
+    //     console.log(doc[0]);
+    // });
+
+    // data.forEach(doc => {
+    //     // console.log(doc.PERGUNTA);
+    //     db.collection('questions').where('PERGUNTA', '==', doc.PERGUNTA).get()
+    //         .then(snapshot => {
+    //             snapshot.forEach(doc => {
+    //                 console.log(doc.id);
+    //             });
+    //         })
+    //         .catch(error => console.log(error));
+    // });
+
+    // const promises = jsonBkp.map(doc => {
+    //     if(doc.edited && doc.edited.length > 0) {
+    //         const iq = doc.edited[0].question.split('-')[0];
+
+    //         prodData[iq] = { ...doc, IQ: iq }
+    //     } else {
+    //         const question = doc.PERGUNTA;
+
+    //         dataRDB.forEach(docRDB => {
+    //             const iq = docRDB[0];
+
+    //             if(question === docRDB[1].PERGUNTA) {
+    //                 prodData[iq] = { ...doc, IQ: iq }
+    //             }
+    //         });
+    //     }
+    // });
+
+    // await Promise.all(promises);
+
+    // const prodTimestamp = '2020-11-25T16-23-01.553Z';
+    // const prodPath = `../../backups/${prodTimestamp}/firestore-prod`;
+    // const prodJson = JSON.stringify(prodData);
+
+    // fs.mkdirSync(prodPath, { recursive: true });
+    // fs.writeFileSync(`${prodPath}/prod-questions.json`, prodJson);
+
+    // data.forEach(doc => {
+    //     recDB.collection('rdb-questions').doc(doc[0]).set(doc[1])
+    //         .catch(console.log);
     // });
 
     // await db.collection('realtime-questions').get()
     //     .then(snapshot => {
     //         console.log('Length: ' + snapshot.docs.length);
     //     }).catch(console.log);
-
-    await db.collection('edited questions').where('IQ', '==', 'testquestion-1').get()
-        .then(snapshot => {
-            snapshot.forEach(doc => console.log(doc.id));
-        })
-        .catch(console.log);
 
     res.send('Aux questions created!');
 }
