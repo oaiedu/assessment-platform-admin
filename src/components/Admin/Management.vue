6f0ed7fa1381926fccdc4ffde72dcb23370429a4
Michell Algarra <michell.algarra@gmail.com>
2020-12-07 18:22:12 -0300

    feat(components/Admin/Management): Created a page to the administrator

---
+++ src/components/Admin/Management.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <div class="text-center">
+            <v-btn color="green" dark fab @click="backup()">
+                <v-icon>mdi-cloud-upload</v-icon>
+            </v-btn>
+        </div>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Management',
+        methods: {
+            backup() {
+                const now = new Date().toISOString().replace(/:/g, '-');
+                this.$store.dispatch('backupFirebase', { now });
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
