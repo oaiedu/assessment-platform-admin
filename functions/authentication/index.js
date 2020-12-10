e3d460a84790e4fc354848cf76ac75836fc9113b
Michell Algarra <michell.algarra@gmail.com>
2020-12-10 15:44:04 -0300

    feat(functions/authentication): Created a function to set the default user claims when it's created

--- functions/authentication/index.js
+++ functions/authentication/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
-const { deleteAuth, importAuth } = require('./userManagement');
+const { deleteAuth, importAuth, setAdminRole, userDefaultRole } = require('./userManagement');
 const { copyAuth } = require('./dataCopy');
 
 module.exports = {
     copyAuth: functions.https.onRequest(copyAuth),
     deleteAuth: functions.https.onRequest(deleteAuth),
-    importAuth: functions.https.onRequest(importAuth)
+    importAuth: functions.https.onRequest(importAuth),
+    setAdminRole: functions.https.onRequest(setAdminRole),
+    userDefaultRole: functions.https.onRequest(userDefaultRole)
 }
