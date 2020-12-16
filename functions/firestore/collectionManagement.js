a23f3ec79af5ae16f22f52edb93c06f776b89ffe
Michell Algarra <michell.algarra@gmail.com>
2020-12-16 18:55:31 -0300

    feat(functions/importFirestore): Added the decodeURIComponent method to each text field that was necessary

--- functions/firestore/collectionManagement.js
+++ functions/firestore/collectionManagement.js
@@ -98,9 +98,10 @@ exports.deleteNonexistentReferences = async (req, res) => {
 
 exports.importFirestore = async (req, res) => {
     const folderRootName = 'backups';
-    const timestamp = '2020-11-27T16-43-04.696Z';
+    const timestamp = '2020-12-09T19-40-40.732Z';
     const path = `../../${folderRootName}/${timestamp}/firestore`;
     const collections = ['users', 'questions', 'question requests', 'edited questions', 'tests', 'papers'];
+    // const collections = ['users'];
 
     collections.forEach(cn => {
         const jsonPath = `${path}/${cn}-${timestamp}.json`;
@@ -109,10 +110,48 @@ exports.importFirestore = async (req, res) => {
         const data = [];
 
         for (const key in json) {
-            data.push([key, json[key]]);
-        }
+            if(cn.includes('question')) {
+                const toPush = {
+                    ...json[key],
+                    PERGUNTA: decodeURIComponent(json[key].PERGUNTA),
+                    DISCIPLINA: decodeURIComponent(json[key].DISCIPLINA),
+                    RESPOSTAS: json[key].RESPOSTAS.map(ans => {
+                        return {
+                            ...ans,
+                            text: decodeURIComponent(ans.text)
+                        }
+                    })
+                }
 
-        console.log(cn + ': ' + json.length);
+                if(cn === 'question requests') {
+                    toPush.NOME = decodeURIComponent(json[key].NOME);
+                }
+
+                data.push([key, toPush]);
+            } else if(cn.includes('papers')) {
+                data.push([key, {
+                    image: json[key].image,
+                    description: decodeURIComponent(json[key].description),
+                    name: decodeURIComponent(json[key].name)
+                }]);
+            } else if(cn.includes('tests')) {
+                data.push([key, {
+                    ...json[key],
+                    created: decodeURIComponent(json[key].created),
+                    purpose: decodeURIComponent(json[key].purpose),
+                    title: decodeURIComponent(json[key].title),
+                    user: decodeURIComponent(json[key].user)
+                }])
+            } else if(cn.includes('users')) {
+                data.push([key, {
+                    email: decodeURIComponent(json[key].email),
+                    name: decodeURIComponent(json[key].name),
+                    profileImages: json[key].profileImages
+                }])
+            } else {
+                data.push([key, json[key]]);
+            }
+        }
 
         data.forEach(doc => {
             db.collection(cn).doc(doc[0]).set(doc[1]).catch(error => console.log(error));
@@ -120,238 +159,4 @@ exports.importFirestore = async (req, res) => {
     });
 
     res.send('Firestore data imported!');
-}
-
-exports.importQuestions = async (req, res) => {
-    const folderRootName = 'backups';
-    const timestamp = '2020-11-27T16-43-04.696Z';
-    const path = `../../${folderRootName}/${timestamp}/firestore`;
-
-    const jsonPath = `${path}/prod-questions-${timestamp}.json`;
-    const file = fs.readFileSync(jsonPath);
-    const json = JSON.parse(file);
-    const data = [];
-
-    for (const key in json) {
-        data.push([key, json[key]]);
-    }
-
-    db.collection('questions').get().then(snapshot => console.log(snapshot.docs.length)).catch(console.log);
-
-    res.send('Firestore questions imported!');
-}
-
-exports.compareQuestions = async (req, res) => {
-    const auxQuestions = [];
-    const iqs = [];
-    const updatedQuestions = [];
-    const subjects = [
-        "Teoria do Reator",
-        "Termodinâmica",
-        "Instrumentação e Controle",
-        "Válvulas e Bombas",
-        "Eletricidade",
-        "Mecânica dos Fluidos",
-        "Tratamento Qúimico Refrigerante",
-        "Análise Integrada",
-        "Instrumentação Nuclear",
-        "Física Nuclear",
-        "Transferência de Calor",
-        "Materiais"
-    ]
-
-    await db.collection('questions').get()
-        .then(snapshot => {
-            snapshot.forEach(doc => {
-                auxQuestions.push(doc.data());
-            });
-        })
-        .catch(console.log);
-
-
-
-
-    await Promise.all(promises);
-
-    console.log(updatedQuestions.length);
-    // updatedQuestions.forEach(doc => {
-    //     recDB.collection('updated-questions').doc(doc.IQ).set(doc).catch(console.log);
-    // });
-
-    res.send('Done!');
-}
-
-exports.auxQuestions = async (req, res) => {
-    const folderRootName = 'rdb-backups';
-    const timestamp = '2020-11-27T16-43-04.696Z';
-    const path = `../../${folderRootName}/${timestamp}`;
-
-    // const jsonPath = `${path}/rdb-${timestamp}.json`;
-    // const file = fs.readFileSync(jsonPath);
-    // const json = JSON.parse(file);
-    // const dataRDB = [];
-
-    const fileBkp = fs.readFileSync('../../backups/2020-11-25T16-23-01.553Z/firestore/questions-2020-11-25T16-23-01.553Z.json');
-    const jsonBkp = JSON.parse(fileBkp);
-    const dataBkp = [];
-
-    const prodTimestamp = '2020-11-25T16-23-01.553Z';
-    const prodPath = `../../backups/${prodTimestamp}/firestore-prod/prod-questions.json`;
-    const prodData = [];
-
-    const fileProd = fs.readFileSync(prodPath);
-    const jsonProd = JSON.parse(fileProd);
-
-    // for(let key in jsonProd) {
-    //     prodData.push(jsonProd[key].PERGUNTA);
-    // }
-
-    // let counter = 0;
-    // const data = {}
-    // const ids = ['QEEDyKtJgThv5uMUqqBd',
-    //             'KuE7bG33h9XxVZdJcen6',
-    //             'OOcUZZqb5IjE3zw4JGBt',
-    //             'LPYfKKJa54wtpiBXGRzp',
-    //             '67YprFZaR5y5jxdzs8JU',
-    //             '5wIk9F5usNKW3iqpWT8T',
-    //             '1kxbcnj0iv8g95aMSRGR',
-    //             '3WwbSjddmYDm6xKbxN0d',
-    //             '0hW2yksx9Ni7nkM8Qglt',
-    //             'qtj3BcWeBKTeDCjodNYk',
-    //             '2pptYsIPVFg0qKYGRQCx']
-
-    // const promises = jsonBkp.map(doc => {
-    //     if(!(doc.edited && doc.edited.length > 0)) {
-    //         if(!prodData.includes(doc.PERGUNTA)) {
-    //             data[ids[counter++]] = doc;
-    //         }
-    //     }
-    // });
-
-    // Promise.all(promises);
-
-    // const lostData = JSON.stringify(data);
-    // fs.writeFileSync('../../backups/lost-questions.json', lostData);
-
-    // for (const key in jsonBkp) {
-    //     // dataBkp.push([key, jsonBkp[key]]);
-    // }
-
-    // console.log('Prod length: ' + prodData.length);
-
-    // prodData.forEach(doc => {
-    //     db.collection('questions').doc(doc[0]).set(doc[1]).catch(error => console.log(error));
-    // });
-
-    // const p3633 = {
-    //     CONHECIMENTO: 'C1.12',
-    //     DISCIPLINA: 'Transferência de Calor',
-    //     IMAGENS: '',
-    //     IQ: 'P3633',
-    //     PERGUNTA: 'O conjunto turbina + gerador principal opera com 80% de carga. O trocador de calor do sistema de óleo lubrificante da turbina principal apresentava as seguintes temperaturas iniciais em regime permanente: Temperatura do óleo lubrificante na entrada do trocador (To-e) = 79ºC Temperatura do óleo lubrificante na saída do trocador (To-s) = 45ºC Temperatura da água de resfriamento na entrada do trocador (Ta-e) = 29ºC Temperatura da água de resfriamento na saída do trocador (Ta-s) = 46ºC Passaram-se seis meses de operação. O trocador de calor do sistema de óleo lubrificante da turbina principal apresenta agora as seguintes temperaturas <u>atuais</u> em regime permanente: To-e = 82ºC To-s = 48ºC Ta-e = 29ºC Ta-s = 46ºC Presuma que ao longo dos seis meses de operação as vazões e os calores específicos da água de resfriamento e do óleo lubrificante permaneceram inalteradas. Qual das seguintes situações pode ter sido responsável pela diferença observada no trocador de calor entre as temperaturas em regime permanente iniciais e atuais?',
-    //     RELEVANCIA_OR: '2.5',
-    //     RELEVANCIA_OSR: '2.7',
-    //     RESPOSTAS: [
-    //         {
-    //             ansId: 'radio-1',
-    //             text: 'Houve crescimento de incrustação nos tubos do trocador de calor.',
-    //             value: true
-    //         },
-    //         {
-    //             ansId: 'radio-2',
-    //             text: 'A temperatura da fonte de água de resfriamento aumentou.',
-    //             value: false
-    //         },
-    //         {
-    //             ansId: 'radio-3',
-    //             text: 'Aumentou a carga sobre o conjunto turbina + gerador principal.',
-    //             value: false
-    //         },
-    //         {
-    //             ansId: 'radio-4',
-    //             text: 'Diminuiu a carga sobre o conjunto turbina + gerador principal.',
-    //             value: false
-    //         }
-    //     ],
-    //     edited: []
-    // }
-
-    // jsonBkp['P3633'] = p3633;
-
-    // const prodBkp = JSON.stringify(jsonBkp);
-
-    // fs.writeFileSync(`../../backups/2020-11-27T16-43-04.696Z/firestore/prod-questions-${timestamp}.json`, prodBkp);
-
-    // for (const key in json) {
-    //     dataRDB.push([key, json[key]]);
-    // }
-
-    // prodData.push(...Object.values(jsonProd));
-
-    // const updated = prodData.map(doc => doc.PERGUNTA);
-
-    // Promise.all(updated);
-
-    // const data = [];
-    // dataRDB.forEach(doc => {
-    //     if(!updated.includes(doc[1].PERGUNTA)) {
-    //         data.push(doc);
-    //         console.log(doc[0]);
-    //     }
-    // });
-    // console.log(data.length);
-
-    // data.forEach(doc => {
-    //     console.log(doc[0]);
-    // });
-
-    // data.forEach(doc => {
-    //     // console.log(doc.PERGUNTA);
-    //     db.collection('questions').where('PERGUNTA', '==', doc.PERGUNTA).get()
-    //         .then(snapshot => {
-    //             snapshot.forEach(doc => {
-    //                 console.log(doc.id);
-    //             });
-    //         })
-    //         .catch(error => console.log(error));
-    // });
-
-    // const promises = jsonBkp.map(doc => {
-    //     if(doc.edited && doc.edited.length > 0) {
-    //         const iq = doc.edited[0].question.split('-')[0];
-
-    //         prodData[iq] = { ...doc, IQ: iq }
-    //     } else {
-    //         const question = doc.PERGUNTA;
-
-    //         dataRDB.forEach(docRDB => {
-    //             const iq = docRDB[0];
-
-    //             if(question === docRDB[1].PERGUNTA) {
-    //                 prodData[iq] = { ...doc, IQ: iq }
-    //             }
-    //         });
-    //     }
-    // });
-
-    // await Promise.all(promises);
-
-    // const prodTimestamp = '2020-11-25T16-23-01.553Z';
-    // const prodPath = `../../backups/${prodTimestamp}/firestore-prod`;
-    // const prodJson = JSON.stringify(prodData);
-
-    // fs.mkdirSync(prodPath, { recursive: true });
-    // fs.writeFileSync(`${prodPath}/prod-questions.json`, prodJson);
-
-    // data.forEach(doc => {
-    //     recDB.collection('rdb-questions').doc(doc[0]).set(doc[1])
-    //         .catch(console.log);
-    // });
-
-    // await db.collection('realtime-questions').get()
-    //     .then(snapshot => {
-    //         console.log('Length: ' + snapshot.docs.length);
-    //     }).catch(console.log);
-
-    res.send('Aux questions created!');
 }
