e1c1d1dd0ee2da9a47aa3459908ee0543a8bfb99
Michell Algarra <michell.algarra@gmail.com>
2020-12-16 18:49:20 -0300

    feat(functions/backup): Created functions to create, download and delete backups in/from Google Drive

--- functions/backup/index.js
+++ functions/backup/index.js
@@ -1,7 +1,17 @@
 const functions = require('firebase-functions');
-const { backupFirebase, backupFirestoreAuth } = require('./backupFirebase');
+
+const {
+    backupFirebase,
+    backupFirestoreAuth,
+    deleteBackup,
+    downloadBackup,
+    testAPI
+} = require('./backupFirebase');
 
 module.exports = {
     backupFirebase: functions.https.onRequest(backupFirebase),
-    backupFirestoreAuth: functions.https.onRequest(backupFirestoreAuth)
+    backupFirestoreAuth: functions.https.onRequest(backupFirestoreAuth),
+    deleteBackup: functions.https.onRequest(deleteBackup),
+    downloadBackup: functions.https.onRequest(downloadBackup),
+    testAPI: functions.https.onRequest(testAPI)
 }
