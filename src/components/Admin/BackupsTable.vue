08671e90b6bb8c82c4182c06694c8aef552c9073
Michell Algarra <michell.algarra@gmail.com>
2020-12-16 18:57:21 -0300

    faet(admin/BackupsTable): Created a component to show the last months backups into a data table

---
+++ src/components/Admin/BackupsTable.vue
@@ -0,0 +1,238 @@
+<template>
+    <v-container>
+        <v-data-table
+            :items='getBackupsByMonth(currentMonth)'
+            class="elevation-1 mt-10"
+            :headers="headers"
+            :loading="loading"
+            :sort-by="['start']"
+            :sort-desc="[true]"
+            loading-text="Carregando backups..."
+            no-data-text="Não há backups neste mês" >
+            <template v-slot:top>
+                <v-toolbar flat color="blue">
+                    <v-toolbar-title class="white--text">Mês Atual</v-toolbar-title>
+                </v-toolbar>
+            </template>
+            <template v-slot:[`item.actions`]='{ item }'>
+                <v-icon
+                    color="green"
+                    @click="downloadBkp(item)" >
+                    mdi-download
+                </v-icon>
+                <v-icon
+                    class="ml-1"
+                    color="red"
+                    @click="deleteBkp(item)" >
+                    mdi-delete
+                </v-icon>
+            </template>
+        </v-data-table>
+
+        <v-data-table
+            :items='getBackupsByMonth(lastMonth)'
+            class="elevation-1 mt-10"
+            :headers="headers"
+            :loading="loading"
+            :sort-by="['start']"
+            :sort-desc="[true]"
+            loading-text="Carregando backups..."
+            no-data-text="Não há backups neste mês" >
+            <template v-slot:top>
+                <v-toolbar flat color="blue">
+                    <v-toolbar-title class="white--text">{{ pastMonths[0] }}</v-toolbar-title>
+                </v-toolbar>
+            </template>
+            <template v-slot:[`item.actions`]='{ item }'>
+                <v-icon
+                    color="green"
+                    @click="downloadBkp(item)" >
+                    mdi-download
+                </v-icon>
+                <v-icon
+                    class="ml-1"
+                    color="red"
+                    @click="deleteBkp(item)" >
+                    mdi-delete
+                </v-icon>
+            </template>
+        </v-data-table>
+
+        <v-data-table
+            :items='getBackupsByMonth(twoMonthsAgo)'
+            class="elevation-1 mt-10"
+            :headers="headers"
+            :loading="loading"
+            :sort-by="['start']"
+            :sort-desc="[true]"
+            loading-text="Carregando backups..."
+            no-data-text="Não há backups neste mês" >
+            <template v-slot:top>
+                <v-toolbar flat color="blue">
+                    <v-toolbar-title class="white--text">{{ pastMonths[1] }}</v-toolbar-title>
+                </v-toolbar>
+            </template>
+            <template v-slot:[`item.actions`]='{ item }'>
+                <v-tooltip top>
+                    <template v-slot:activator='{ on, attrs }'>
+                        <v-icon
+                            v-on='on'
+                            v-bind='attrs'
+                            color="green"
+                            @click="downloadBkp(item)" >
+                            mdi-download
+                        </v-icon>
+                    </template>
+                    <span>Editar função</span>
+                </v-tooltip>
+
+                <v-tooltip top>
+                    <template v-slot:activator='{ on, attrs }'>
+                        <v-icon
+                            v-on='on'
+                            v-bind='attrs'
+                            class="ml-1"
+                            color="red"
+                            @click="deleteQuestionSnackBar = true; deleteItem = item" >
+                            mdi-delete
+                        </v-icon>
+                    </template>
+                    <span>Editar função</span>
+                </v-tooltip>
+            </template>
+        </v-data-table>
+
+        <v-snackbar
+                v-model="deleteQuestionSnackBar"
+                color="white"
+                right
+                top
+                :timeout="15000" >
+                <span class="snackbar-text">Tem certeza de que deseja excluir este Backup?</span>
+
+                <v-btn
+                    dark
+                    class="ml-3"
+                    color="blue"
+                    text
+                    @click="deleteBkp(deleteItem)" >
+                    Excluir
+                </v-btn>
+
+                <v-btn
+                    dark
+                    color="grey"
+                    text
+                    @click="deleteQuestionSnackBar = false; deleteItem = null" >
+                    Cancelar
+                </v-btn>
+            </v-snackbar>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'BackupsTable',
+        data() {
+            return {
+                deleteQuestionSnackBar: false,
+                deleteItem: null,
+                months: {},
+                headers: [
+                    {
+                        text: 'ID',
+                        value: 'id',
+                        align: 'center',
+                        sortable: true
+                    },
+                    {
+                        text: 'Tamanho',
+                        value: 'size',
+                        align: 'center',
+                        sortable: true
+                    },
+                    {
+                        text: 'Data de Início',
+                        value: 'start',
+                        align: 'center',
+                        sortable: true
+                    },
+                    {
+                        text: 'Data de Término',
+                        value: 'end',
+                        align: 'center',
+                        sortable: true
+                    },
+                    {
+                        text: 'Ações',
+                        value: 'actions',
+                        align: 'center',
+                        sortable: false
+                    }
+                ]
+            }
+        },
+        computed: {
+            backups() {
+                return this.$store.getters.getBackups;
+            },
+            loading() {
+                return this.$store.getters.loading;
+            },
+            currentMonth() {
+                return new Date().getMonth() + 1;
+            },
+            lastMonth() {
+                return this.currentMonth - 1 === 0 ? 12 : this.currentMonth - 1;
+            },
+            twoMonthsAgo() {
+                return this.currentMonth - 2 === 0 ? 12 : this.currentMonth - 2;
+            },
+            pastMonths() {
+                return [this.months[this.lastMonth], this.months[this.twoMonthsAgo]];
+            }
+        },
+        methods: {
+            getBackupsByMonth(month) {
+                const backups = this.backups;
+                const bkps = [];
+                const wanted = month;
+
+                backups.forEach(bkp => {
+                    const start = bkp.start;
+                    const bkpMon = start.substr(0, 3);
+                    const months = [];
+
+                    for (const key in this.months) {
+                        const mon = this.months[key].substr(0, 3);
+
+                        if(bkpMon === mon) {
+                            months.push(key);
+                        }
+                    }
+
+                    months.forEach(m => {
+                        if(m == wanted) {
+                            bkps.push(bkp);
+                        }
+                    });
+                });
+
+                return bkps;
+            },
+            downloadBkp(backup) {
+                this.$store.dispatch('downloadBackup', { date: backup.start, cloudId: backup.cloudId, id: backup.id });
+            },
+            deleteBkp(backup) {
+                this.$store.dispatch('deleteBackup', { id: backup.cloudId });
+            }
+        },
+        mounted() {
+            this.months = this.$store.getters.getMonths;
+        }
+    }
+</script>
+
+<style>
+
+</style>
