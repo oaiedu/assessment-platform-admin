ca0bcb70309ee20ab9740280a0e62a490bdb6df4
Michell Algarra <michell.algarra@gmail.com>
2020-12-16 19:16:31 -0300

    feat(DisplayProfile): Added the role name in profile card

--- src/components/DisplayProfile.vue
+++ src/components/DisplayProfile.vue
@@ -14,6 +14,7 @@
       <v-row justify="start">
         <v-card-title class="pb-0">{{user.name}}</v-card-title>
         <v-card-text>{{user.email}}</v-card-text>
+        <v-card-subtitle>{{ getRoleName(user.role) }}</v-card-subtitle>
       </v-row>
     </v-container>
   </v-card>
@@ -24,6 +25,14 @@ export default {
     computed: {
         user() {
             return this.$store.getters.userInfo
+        }
+    },
+    methods: {
+        getRoleName(role) {
+            if(role === 'admin') return 'Administrador';
+            else if(role === 'appraiser') return 'Avaliador';
+            else if(role === 'teacher') return 'Professor';
+            else return 'Aluno';
         }
     }
 }
