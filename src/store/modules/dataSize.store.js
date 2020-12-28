9d9bf21dfa7c38c25b9694864a447b78539fc29c
Michell Algarra <michell.algarra@gmail.com>
2020-12-28 16:19:09 -0300

    feat(store/dataSize): Created a store module to contain all the data and methods about data amount

---
+++ src/store/modules/dataSize.store.js
@@ -0,0 +1,65 @@
+import { db } from '../../main';
+
+const initialState = () => ({
+    dataSize: null
+});
+
+const state = initialState();
+
+const mutations = {
+    setDataSize (state, data) {
+        state.dataSize = data;
+    },
+    addRemoveSize(state, data) {
+        state.dataSize[data.key] = data.data;
+    },
+    RESETDataSize(state) {
+        const newState = initialState();
+        Object.keys(newState).forEach(key => {
+            state[key] = newState[key];
+        });
+    }
+}
+
+const actions = {
+    loadDataSize({ commit }) {
+        commit('setLoading', true);
+
+        const data = {};
+
+        db.collection('data-size').get()
+            .then(snapshot => {
+                const doc = snapshot.docs[0];
+
+                for(let key in doc.data()) {
+                    data[key] = doc.data()[key];
+                }
+            })
+            .then(() => {
+                commit('setDataSize', data);
+                commit('setLoading', false);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    resetDataSize({ commit }) {
+        commit('RESETDataSize');
+    }
+}
+
+const getters = {
+    getDataSize(state) {
+        return state.dataSize;
+    },
+    getNumberOfQuestionBySubject: state => subjectName => {
+        return state.dataSize.questions.subject[subjectName];
+    },
+}
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
