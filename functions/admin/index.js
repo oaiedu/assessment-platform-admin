8b151a36e2c3379a3f81cc936e0de8467a848303
Michell Algarra <michell.algarra@gmail.com>
2021-01-07 14:06:54 -0300

    refactor(functions/admin): Just commented some lines to not throw a deploy error

--- functions/admin/index.js
+++ functions/admin/index.js
@@ -1,24 +1,24 @@
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-const secondaryAdmin = admin.initializeApp(require('../../.env'), 'secondaryAdmin');
+// const secondaryAdmin = admin.initializeApp(require('../../.env'), 'secondaryAdmin');
 
 const db = admin.firestore();
 const storage = admin.storage().bucket();
 const auth = admin.auth();
 
-const secDB = secondaryAdmin.firestore();
-const secStorage = secondaryAdmin.storage().bucket();
-const secAuth = secondaryAdmin.auth();
+// const secDB = secondaryAdmin.firestore();
+// const secStorage = secondaryAdmin.storage().bucket();
+// const secAuth = secondaryAdmin.auth();
 
-const secRDB = secondaryAdmin.database();
+// const secRDB = secondaryAdmin.database();
 
 module.exports = {
     db,
     storage,
     auth,
-    secDB,
-    secStorage,
-    secAuth,
-    secRDB
+    // secDB,
+    // secStorage,
+    // secAuth,
+    // secRDB
 }
