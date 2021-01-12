662e87e8c3441968e9064ae36f9e6469fee6f2e2
Michell Algarra <michell.algarra@gmail.com>
2021-01-12 18:04:20 -0300

    style(components/Error): Just removed some logs and organized the code

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,24 +1,28 @@
 <template>
-  <div style="position: absolute; left: 50%;" class="pt-4">
-    <v-alert style="position: relative; left: -50%;" type="error" @dismiss="onDismissed" v-if="error" dismissible>{{error.message}}</v-alert>
-  </div>
+    <div style="position: absolute; left: 50%;" class="pt-4">
+        <v-alert
+            v-if="error"
+            style="position: relative; left: -50%;"
+            type="error"
+            @dismiss="onDismissed"
+            dismissible >
+            {{ error.message }}
+        </v-alert>
+    </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-
-    }),
-    computed: {
-      error () {
-          return this.$store.getters.error
-      },
-    },
-    methods: {
-      onDismissed () {
-          console.log('Dismissed Alert!')
-          this.$store.dispatch('clearError')
-      }
+    export default {
+        name: 'Error',
+        computed: {
+            error () {
+                return this.$store.getters.error
+            },
+        },
+        methods: {
+            onDismissed () {
+                this.$store.dispatch('clearError');
+            }
+        }
     }
-  }
 </script>
