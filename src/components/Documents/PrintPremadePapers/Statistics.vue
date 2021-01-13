c8b9b04f2c82dcc9bac0748b6ed01c581b7eebe4
Michell Algarra <michell.algarra@gmail.com>
2021-01-13 17:13:53 -0300

    feat(documents/Statistics): Created a Statistics template document

---
+++ src/components/Documents/PrintPremadePapers/Statistics.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-container>
+        <div class="question-page">
+            <statistics-questions :statistics="statistics" :numberOfQuestions="numberOfQuestions"/>
+        </div>
+
+        <v-tooltip right>
+            <template v-slot:activator='{ on, attrs }'>
+                <v-btn
+                    v-on="on"
+                    v-bind="attrs"
+                    fixed
+                    fab
+                    bottom
+                    left
+                    @click="back()" >
+                    <v-icon>mdi-arrow-left</v-icon>
+                </v-btn>
+            </template>
+            <span>Voltar</span>
+        </v-tooltip>
+    </v-container>
+</template>
+
+<script>
+    import StatisticsQuestions from '@/components/Questions/StatisticsQuestions';
+
+    export default {
+        name: 'PPStatistics',
+        components: { StatisticsQuestions },
+        computed: {
+            statistics() {
+                let statisticsObj = [];
+                const subject =  this.$store.getters.getSubjects;
+                subject.forEach(element => {
+                    const numberOfQuestions = 0;
+                    statisticsObj.push({ name: element, questions: numberOfQuestions });
+                })
+                return statisticsObj;
+            },
+            numberOfQuestions() {
+                return 0;
+            },
+        },
+        methods: {
+            back() {
+                this.$emit('closeDialogStatistics');
+            }
+        }
+    }
+</script>
