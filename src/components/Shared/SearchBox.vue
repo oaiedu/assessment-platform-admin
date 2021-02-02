787ddde058c9813a4d0a141974a378f828e196a9
Michell Algarra <michell.algarra@gmail.com>
2021-02-02 15:27:02 -0300

    feat(shared/SearchBox): Created a component to be a searching text field that can be used by any component

---
+++ src/components/Shared/SearchBox.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-text-field
+        id='searchField'
+        v-model="search"
+        @keydown="keydown($event)"
+        clearable
+        filled
+        rounded
+        dense
+        append-icon="mdi-magnify"
+        :label="label"
+        single-line
+        hide-details >
+    </v-text-field>
+</template>
+
+<script>
+    export default {
+        name: 'SearchBox',
+        props: { label: String },
+        data() {
+            return {
+                search: ''
+            }
+        },
+        methods: {
+            keydown(event) {
+                if(event.key === 'Enter') {
+                    document.getElementById('searchField').blur();
+                    this.$emit('enter', this.search);
+                }
+            }
+        },
+        watch: {
+            search(text) {
+                this.$emit('textChange', text);
+            }
+        }
+    }
+</script>
