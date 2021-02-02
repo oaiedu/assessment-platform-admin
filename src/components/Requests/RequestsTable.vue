9d47d1729d37a54971cdf6c01ff2017c32456843
Michell Algarra <michell.algarra@gmail.com>
2021-02-02 15:34:28 -0300

    feat(data-table): Created a component to be the data table of each type of data

---
+++ src/components/Requests/RequestsTable.vue
@@ -0,0 +1,190 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-data-table
+                :headers="headers"
+                :items="items"
+                :page="page"
+                :items-per-page="itemsPerPage"
+                :loading="loading"
+                loading-text="Carregando solicitações..."
+                no-data-text="Não há solicitações"
+                hide-default-footer
+                :item-class="itemRowStyle"
+                class="elevation-1" >
+                <template v-slot:[`item.status`]='{ item }' v-if="items && items.length > 0">
+                    <span
+                        :style='{ "color": item.status === "Pendente"
+                            ? "#ffaa00" : item.status === "Aprovado"
+                            ? "#00cc66" : "#ff0000" }'
+                        >{{ item.status }}</span>
+                </template>
+
+                <template v-slot:[`item.actions`]="{ item }">
+                    <v-row justify="end" v-if='!item.toDelete'>
+                        <v-tooltip top v-if='userClaims["admin"]'>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    @click="onEmailClick(item)" >
+                                    mdi-email
+                                </v-icon>
+                            </template>
+                            <span>Enviar e-mail</span>
+                        </v-tooltip>
+
+                        <v-tooltip top>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    class="ml-3"
+                                    @click="onPdfClick(item)" >
+                                    mdi-pdf-box
+                                </v-icon>
+                            </template>
+                            <span>Visualizar PDF</span>
+                        </v-tooltip>
+
+                        <v-tooltip top v-if='userClaims["admin"]'>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    class="ml-3"
+                                    :disabled='item.status === "Aprovado"'
+                                    @click="onCheckClick(item)" >
+                                    mdi-check-bold
+                                </v-icon>
+                            </template>
+                            <span>Aprovar</span>
+                        </v-tooltip>
+
+                        <v-tooltip top v-if='userClaims["appraiser"]'>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    class="ml-3"
+                                    :disabled='item.status === "Aprovado"'
+                                    @click="onEditClick(item)" >
+                                    mdi-pencil
+                                </v-icon>
+                            </template>
+                            <span>Editar</span>
+                        </v-tooltip>
+
+                        <v-tooltip top>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    class="ml-3"
+                                    :disabled='userClaims["admin"] ? item.status === "Rejeitado" : item.status === "Aprovado"'
+                                    @click="userClaims['admin']
+                                        ? onRejectClick(item)
+                                        : onDeleteClick(item)" >
+                                    {{ userClaims['admin'] ? 'mdi-close' : 'mdi-delete' }}
+                                </v-icon>
+                            </template>
+                            <span>{{ userClaims['admin'] ? 'Rejeitar' : 'Excluir' }}</span>
+                        </v-tooltip>
+                    </v-row>
+
+                    <v-row justify="end" v-else-if='item.toDelete && item.toDelete.status'>
+                        <v-btn
+                            style="padding: 0 !important; font-weight: bold !important;"
+                            color='red'
+                            text
+                            :disabled="!userClaims['appraiser']"
+                            @click='onRestoreClick(item)' >
+                            {{ userClaims['appraiser'] ? 'Restaurar' : 'Indisponível' }}
+                        </v-btn>
+                    </v-row>
+
+                    <v-row justify="end" v-else>
+                        <v-btn
+                            style="padding: 0 !important; font-weight: bold !important;"
+                            disabled
+                            text >
+                            Excluída
+                        </v-btn>
+                    </v-row>
+                </template>
+            </v-data-table>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'RequestsTable',
+        props: [
+            'items',
+            'page',
+            'itemsPerPage'
+        ],
+        computed: {
+            headers() {
+                return this.userClaims['admin']
+                    ? [
+                        { text: 'IQ', sortable: true, value: 'iq', align: 'left' },
+                        { text: 'Usuário', value: 'user.name', sortable: true, align: 'center' },
+                        { text: 'E-mail', value: 'user.email', sortable: true, align: 'center' },
+                        { text: 'Disciplina', value: 'subject', sortable: true, align: 'center' },
+                        { text: 'Status', value: 'status', sortable: true, align: 'center' },
+                        { text: 'Ações', value: 'actions', sortable: false, align: 'right' }
+                    ]
+                    : [
+                        { text: 'IQ', sortable: true, value: 'iq', align: 'left' },
+                        { text: 'Disciplina', value: 'subject', sortable: true, align: 'center' },
+                        { text: 'Status', value: 'status', sortable: true, align: 'center' },
+                        { text: 'Ações', value: 'actions', sortable: false, align: 'right' }
+                    ]
+            },
+            loading() {
+                return this.$store.getters.loading;
+            },
+            userClaims() {
+                return this.$store.getters.getUserClaims;
+            }
+        },
+        methods: {
+            itemRowStyle(item) {
+                return item.toDelete ? (item.toDelete.status ? 'item-to-delete' : 'item-deleted') : '';
+            },
+            onEmailClick(item) {
+                this.$emit('emailClick', item.user.email);
+            },
+            onPdfClick(item) {
+                this.$emit('pdfClick', item);
+            },
+            onEditClick(item) {
+                this.$emit('editClick', item);
+            },
+            onCheckClick(item) {
+                this.$emit('checkClick', item);
+            },
+            onRejectClick(item) {
+                this.$emit('rejectClick', item);
+            },
+            onDeleteClick(item) {
+                this.$emit('deleteClick', item);
+            },
+            onRestoreClick(item) {
+                this.$emit('restoreClick', item);
+            }
+        }
+    }
+</script>
+
+<style>
+    .item-to-delete {
+        color: #f00;
+    }
+
+    .item-deleted {
+        color: #c4c4c4;
+    }
+</style>
---
+++ src/components/Tests/TestsTable.vue
@@ -0,0 +1,137 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-data-table
+                :headers='headers'
+                :items="items"
+                :page="page"
+                :items-per-page="itemsPerPage"
+                :loading="loading"
+                no-data-text='Não há provas a serem mostradas'
+                loading-text="Carregando provas..."
+                hide-default-footer
+                :item-class="itemRowStyle"
+                class="elevation-1" >
+                <template v-slot:[`item.type`]='{ item }'>
+                    {{ item.type === 'selected' ? 'Selecionado' : 'Aleatório' }}
+                </template>
+
+                <template v-slot:[`item.actions`]="{ item }">
+                    <v-row justify="end" v-if='!item.toDelete'>
+                        <v-tooltip top>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    @click='onPdfClick(item)' >
+                                    mdi-pdf-box
+                                </v-icon>
+                            </template>
+                            <span>Visualizar PDF</span>
+                        </v-tooltip>
+
+                        <v-tooltip top>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-if='!userClaims["student"]'
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    class="ml-2"
+                                    @click="onEditClick(item)" >
+                                    mdi-pencil
+                                </v-icon>
+                            </template>
+                            <span>Editar</span>
+                        </v-tooltip>
+
+                        <v-tooltip top>
+                            <template v-slot:activator='{ on, attrs }'>
+                                <v-icon
+                                    v-if='!userClaims["student"]'
+                                    v-on='on'
+                                    v-bind='attrs'
+                                    class="ml-2"
+                                    @click='onDeleteClick(item)' >
+                                    mdi-delete
+                                </v-icon>
+                            </template>
+                            <span>Excluir</span>
+                        </v-tooltip>
+                    </v-row>
+
+                    <v-row justify="end" v-else-if='item.toDelete && item.toDelete.status'>
+                        <v-btn
+                            style="padding: 0 !important; font-weight: bold !important;"
+                            color='red'
+                            text
+                            :disabled='userClaims["teacher"] && item.toDelete.userEmail !== userInfo.email'
+                            @click='onRestoreClick(item)' >
+                            {{ userClaims["teacher"] && item.toDelete.userEmail !== userInfo.email ? 'Indisponível' : 'Restaurar' }}
+                        </v-btn>
+                    </v-row>
+
+                    <v-row justify="end" v-else>
+                        <v-btn
+                            style="padding: 0 !important; font-weight: bold !important;"
+                            disabled
+                            text >
+                            Excluída
+                        </v-btn>
+                    </v-row>
+                </template>
+            </v-data-table>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'TestsTable',
+        props: ['items', 'itemsPerPage', 'page'],
+        data() {
+            return {
+                headers: [
+                    { text: "Nome", align: "start",  value: "title" },
+                    { text: "Questões", align: "left",  value: "questions.length" },
+                    { text: "Tipo", align: "left",  value: "type" },
+                    { text: "Ações", align:"right", value: "actions", sortable: false }
+                ]
+            }
+        },
+        computed: {
+            loading() {
+                return this.$store.getters.loading;
+            },
+            userClaims() {
+                return this.$store.getters.getUserClaims;
+            }
+        },
+        methods: {
+            itemRowStyle(item) {
+                return item.toDelete ? (item.toDelete.status ? 'item-to-delete' : 'item-deleted') : '';
+            },
+            onPdfClick(item) {
+                this.$emit('pdfClick', item.id);
+            },
+            onEditClick(item) {
+                this.$emit('editClick', item);
+            },
+            onDeleteClick(item) {
+                this.$emit('deleteClick', item);
+            },
+            onRestoreClick(item) {
+                this.$emit('restoreClick', item);
+            }
+        }
+    }
+</script>
+
+<style>
+    .item-to-delete {
+        color: #f00;
+    }
+
+    .item-deleted {
+        color: #c4c4c4;
+    }
+</style>
