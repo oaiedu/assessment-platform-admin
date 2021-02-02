5729e318440c6b04b9ca0be47993223376c35b33
Michell Algarra <michell.algarra@gmail.com>
2021-02-02 15:32:24 -0300

    feat(DeleteAlert): Created a delete alert component to each type of data

---
+++ src/components/Requests/DeleteAlertRequests.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-alert
+            v-if='confirmCondition'
+            text
+            prominent
+            type='warning'
+            color="red"
+            icon='mdi-alert' >
+            Exclusão confirmada! Quando deixar esta página, a tabela será atualizada.
+        </v-alert>
+
+        <v-alert
+            v-if='itemsCondition'
+            text
+            prominent
+            type='warning'
+            color="red"
+            icon='mdi-alert' >
+            {{ itemsText }}
+            <br>
+            {{ items }}
+        </v-alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DeleteAlertRequests',
+        props:['confirmCondition', 'itemsCondition', 'itemsText', 'items']
+    }
+</script>
---
+++ src/components/Tests/DeleteAlertTests.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-alert
+            v-if='confirmCondition'
+            text
+            prominent
+            type='warning'
+            color="red"
+            icon='mdi-alert' >
+            Exclusão confirmada! Quando deixar esta página, a tabela será atualizada.
+        </v-alert>
+
+        <v-alert
+            v-if='itemsCondition'
+            text
+            prominent
+            type='warning'
+            color="red"
+            icon='mdi-alert' >
+            {{ itemsText }}
+            <br>
+            {{ items }}
+
+
+            <div v-if="isAdmin">
+                <br>
+                Marcadas por você:
+                <br>
+                {{ adminItems }}
+            </div>
+        </v-alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DeleteAlertPapers',
+        props:['confirmCondition', 'itemsCondition', 'itemsText', 'items', 'isAdmin', 'adminItems']
+    }
+</script>
