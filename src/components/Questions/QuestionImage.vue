2e2d6faccd9f7a6dafb71f20b4918dff8cc041f8
Michell Algarra <michell.algarra@gmail.com>
2021-02-04 16:35:45 -0300

    feat(questions/QuestionImage): Create a component to be the question image and apply it's size depending on it's properties

---
+++ src/components/Questions/QuestionImage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="img-container mb-5">
+        <v-container v-if="imageSize === '1x'">
+            <img :src="image" style="max-height: 40vh; max-width: 40vw" />
+        </v-container>
+
+        <v-container v-else-if="imageSize === '2x'">
+            <img :src="image" style="max-height: 50vh; max-width: 50vw" />
+        </v-container>
+
+        <v-container v-else-if="imageSize === '3x'">
+            <img :src="image" style="max-height: 60vh; max-width: 60vw" />
+        </v-container>
+
+        <v-container v-if="imageSize === undefined">
+            <img :src="image" style="max-height: 50vh; max-width: 50vw" />
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Image',
+        props: ['image', 'imageSize']
+    }
+</script>
+
+<style>
+    @media print {
+        .img-container {
+            text-align: center !important;
+        }
+    }
+</style>
