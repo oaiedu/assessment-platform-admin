e6830d72a6dea26f7533be02f0b282e73095b48c
Michell Algarra <michell.algarra@gmail.com>
2021-02-04 16:59:49 -0300

    feat(tests/TestQuestions): Created a component to show all questions from the test

---
+++ src/components/Tests/TestQuestions.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div v-for="(question, index) in questions" :key="question.iq">
+            <div class="question-page">
+                QUESTÃO {{ index + 1 }}
+                <br>
+                <br>
+
+                <Definition :question='question' />
+
+                <CustomImage
+                    v-if='confirmImage(question.image)'
+                    :image='question.image'
+                    :imageSize='question.imageSize' />
+
+                <br>
+
+                <Answers :answers='question.answers' />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Definition from '../Questions/Definition';
+    import CustomImage from '../Questions/QuestionImage';
+    import Answers from '../Questions/Answers';
+
+    export default {
+        name: 'TestQuestions',
+        props: ['questions'],
+        components: { Definition, CustomImage, Answers },
+        methods: {
+            confirmImage(image) {
+                return image && image.length > 0;
+            }
+        }
+    }
+</script>
+
+<style>
+    @media print {
+        .question-page {
+            font-size: 13px;
+            page-break-after: always !important
+        }
+    }
+</style>
