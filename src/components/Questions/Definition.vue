100e99778e2a1f9bee57ed9b0178111471df80c4
Michell Algarra <michell.algarra@gmail.com>
2021-02-04 16:34:51 -0300

    feat(questions/Definition): Create a component to be the header details of a question

---
+++ src/components/Questions/Definition.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        DISCIPLINA: {{ question.subject }}
+        <br />
+        CONHECIMENTO: {{ question.knowledge }} [ {{ question.knowledgePWR }} / {{ question.knowledgeBWR }} ]
+        <br />
+        IQ: {{ question.iq }}
+        <br />
+
+        <br />
+        <vue-markdown :source="question.question" />
+        <br />
+    </div>
+</template>
+
+<script>
+    import VueMarkdown from "vue-markdown";
+    require("vue-markdown");
+
+    export default {
+        name: 'Definition',
+        props: ['question'],
+        components: {
+            'vue-markdown': VueMarkdown
+        }
+    }
+</script>
