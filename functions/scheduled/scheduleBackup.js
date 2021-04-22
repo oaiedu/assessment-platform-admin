0760d96d4ee2343f20d94d8f30cb7acfbb0e5b00
Michell Algarra <michell.algarra@gmail.com>
2021-04-22 17:34:32 -0300

    feat(functions/scheduledBackup): implemented the scheduled backup function

--- functions/scheduled/scheduleBackup.js
+++ functions/scheduled/scheduleBackup.js
@@ -1,4 +1,121 @@
+const axios = require('axios');
+const uuid = require('uuid-random');
+const { db } = require('../admin');
+
+const months = {
+    '1': 'Janeiro',
+    '2': 'Fevereiro',
+    '3': 'Março',
+    '4': 'Abril',
+    '5': 'Maio',
+    '6': 'Junho',
+    '7': 'Julho',
+    '8': 'Agosto',
+    '9': 'Setembro',
+    '10': 'Outubro',
+    '11': 'Novembro',
+    '12': 'Dezembro'
+};
+
 exports.scheduledBackup = async context => {
-    console.log(new Date().toISOString());
-    console.log(context);
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+    const day = date.substr(0, 2);
+    const month = date.substr(3, 2);
+    const year = date.substr(6, 4);
+    const isoString = `${year}-${month}-${day}T${time}.000Z`;
+
+    const createErrorLog = async (type, date, message, payload) => {
+        const id = uuid();
+        const user = {
+            id: 'firebase-functions-cloud',
+            name: 'Firebase Functions',
+            email: 'no@email.com'
+        }
+        const log = {
+            id,
+            type,
+            date,
+            user,
+            message,
+            payload
+        }
+
+        await db.collection('logs').add(log)
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    let url = '';
+
+    if (context && context.name && context.name.includes('stage-pwr-quiz-generator')) {
+        url = 'https://us-central1-stage-pwr-quiz-generator.cloudfunctions.net/backup-backupFirestoreAuth?now=' + isoString.replace(/:/g, '-');
+    } else {
+        url = 'https://us-central1-pwr-quiz-generator.cloudfunctions.net/backup-backupFirestoreAuth?now=' + isoString.replace(/:/g, '-');
+    }
+
+    const bkp = {
+        id: '',
+        size: '',
+        start: '',
+        end: '',
+        cloudId: '',
+        month: ''
+    }
+
+    await axios.get(url)
+        .then(res => {
+            bkp.size = res.data.size;
+            bkp.end = res.data.endDate;
+            bkp.cloudId = res.data.cloudId;
+        })
+        .catch(error => {
+            createErrorLog('Automatic Backup', isoString, error.message, { url });
+        });
+
+    db.collection('backups').get()
+        .then(snapshot => {
+            let lastBkpId;
+            let bkpId;
+
+            if(snapshot.docs.length > 0) {
+                snapshot.forEach(doc => {
+                    const id = parseInt(doc.data().id.substr(3, 4));
+                    if(!lastBkpId) {
+                        lastBkpId = id;
+                    } else {
+                        if(id > lastBkpId) {
+                            lastBkpId = id;
+                        }
+                    }
+                });
+
+                bkpId = lastBkpId + 1;
+            } else bkpId = 1;
+
+            const formatDate = (date) => {
+                const month = new Date(date).getMonth() + 1;
+
+                const dateTime = new Date(date).toString();
+                const sub = dateTime.substr(7, 17);
+                const monthName = months[month].substr(0, 3);
+
+                return monthName + sub;
+            }
+
+            bkp.id = 'ab' + (bkpId >= 1000 ? bkpId : bkpId.toString().padStart(4, '0'));
+            bkp.start = formatDate(isoString);
+            bkp.end = formatDate(bkp.end);
+            bkp.month = bkp.start.substr(0, 3);
+
+            db.collection('backups').add(bkp)
+                .catch(error => {
+                    console.error(error);
+                });
+        })
+        .catch(error => {
+            createErrorLog('Backup DB Insert', isoString , error.message, { bkp });
+        });
 }
