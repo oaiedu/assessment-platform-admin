ffed852c4c79afcf233ffa19552ad747f69dfe1c
Michell Algarra <michell.algarra@gmail.com>
2021-04-23 19:34:11 -0300

    feat(functions/utils): created the utils folder to firebase functions

---
+++ functions/utils/date.js
@@ -0,0 +1,10 @@
+export const getNowISOString = () => {
+    const now = new Date();
+    const time = now.toLocaleTimeString('pt-BR');
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const year = now.getFullYear();
+    const miliseconds = now.getMilliseconds().toString().padStart(3, '0');
+
+    return `${year}-${month}-${day}T${time}.${miliseconds}`;
+}
---
+++ functions/utils/errors.js
@@ -0,0 +1,20 @@
+const uuid = require('uuid-random');
+
+const { getNowISOString } = require('./date');
+
+export const createLog = async (type, message, payload) => {
+    const log = {
+        id: uuid(),
+        type,
+        date: getNowISOString(),
+        user: {
+            id: 'firebase-functions-cloud',
+            name: 'Firebase Functions',
+            email: 'no@email.com'
+        },
+        message,
+        payload
+    }
+
+    return log;
+}
