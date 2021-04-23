f62c1eb748fcb9c7eed393a457b4d6991381b226
Michell Algarra <michell.algarra@gmail.com>
2021-04-23 19:52:37 -0300

    feat(functions/scheduled): added the scheduled delete function to 'index.js'

--- functions/scheduled/index.js
+++ functions/scheduled/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const { scheduledBackup } = require('./scheduleBackup');
+const { scheduledDelete } = require('./scheduleDelete');
 
 module.exports = {
-    scheduledBackup: functions.pubsub.schedule('0 0 * * *').onRun(scheduledBackup)
+    scheduledBackup: functions.pubsub.schedule('0 0 * * *').onRun(scheduledBackup),
+    scheduledDelete: functions.pubsub.schedule('0 1 * * *').onRun(scheduledDelete)
 }
