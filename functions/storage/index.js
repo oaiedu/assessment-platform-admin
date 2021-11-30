1047dc63b804c7e514de1928e3b93bf14d55c999
Michell Algarra <michell.algarra@gmail.com>
2021-11-30 17:23:49 -0300

    feat(functions/index.js): exported the 'downloadFiles' function

--- functions/storage/index.js
+++ functions/storage/index.js
@@ -1,16 +1,20 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const {
     deleteUnusedImages,
     moveEditedQuestionImages,
     movePaperImages,
     moveQuestionImages,
     moveUserImages
-} = require('./directoryManagement');
+} = require("./directoryManagement");
+const { downloadFiles } = require("./downloadStorage");
 
 module.exports = {
     deleteUnusedImages: functions.https.onRequest(deleteUnusedImages),
-    moveEditedQuestionImages: functions.https.onRequest(moveEditedQuestionImages),
+    moveEditedQuestionImages: functions.https.onRequest(
+        moveEditedQuestionImages
+    ),
     movePaperImages: functions.https.onRequest(movePaperImages),
     moveQuestionImages: functions.https.onRequest(moveQuestionImages),
-    moveUserImages: functions.https.onRequest(moveUserImages)
-}
+    moveUserImages: functions.https.onRequest(moveUserImages),
+    downloadFiles: functions.https.onRequest(downloadFiles)
+};
