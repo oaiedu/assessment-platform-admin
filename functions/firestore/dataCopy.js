c916026e54ab6ada60791f063de7cb86d2125669
Michell Algarra <michell.algarra@gmail.com>
2021-11-30 17:23:07 -0300

    refactor(functions/dataCopy): improved function to copy firestore

--- functions/firestore/dataCopy.js
+++ functions/firestore/dataCopy.js
@@ -1,39 +1,47 @@
-const { db, secDB } = require('../admin');
+const { db, secDB } = require("../admin");
 
 exports.copyFirestore = async (req, res) => {
     let dataLength = 0;
     let counter = 0;
 
-    await db.listCollections().then(collections => {
-        collections.forEach(async collection => {
-            const collectionName = collection.id;
+    await secDB
+        .listCollections()
+        .then(collections => {
+            collections.forEach(async collection => {
+                const collectionName = collection.id;
 
-            await secDB.collection(collectionName).get()
-                .then(snapshot => {
-                    dataLength += snapshot.docs.length;
-                    snapshot.forEach(doc => {
-                        counter++;
-                        db.collection(collectionName).doc(doc.id).set(doc.data())
-                            .catch(error => {
-                                console.log('Remaining Error');
-                                console.log(error);
-                            });
+                await secDB
+                    .collection(collectionName)
+                    .get()
+                    .then(snapshot => {
+                        dataLength += snapshot.docs.length;
+                        snapshot.forEach(doc => {
+                            counter++;
+                            db.collection(collectionName)
+                                .doc(doc.id)
+                                .set(doc.data())
+                                .catch(error => {
+                                    console.log("Remaining Error");
+                                    console.log(error);
+                                });
+                        });
+
+                        return snapshot;
+                    })
+                    .catch(error => {
+                        console.log("Secondary DB Error");
+                        console.log(error);
                     });
+            });
 
-                    return snapshot;
-                })
-                .catch(error => {
-                    console.log('Secondary DB Error');
-                    console.log(error);
-                });
+            return collections;
+        })
+        .catch(error => {
+            console.log("DB Error");
+            console.log(error);
         });
 
-        return collections;
-    })
-    .catch(error => {
-        console.log('DB Error');
-        console.log(error);
-    });
-
-    res.send(`Origin Data Length: ${dataLength}\nCopied Data Length: ${counter}`);
-}
+    res.send(
+        `Origin Data Length: ${dataLength}\nCopied Data Length: ${counter}`
+    );
+};
