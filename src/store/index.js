c4b69be8658d4f5b190f192e84994fb7a5b2694a
Michell Algarra <michell.algarra@gmail.com>
2022-01-17 15:38:51 -0300

    feat(store/index.js): removed the vuex persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,17 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import VuexPersist from 'vuex-persist';
+import Vue from "vue";
+import Vuex from "vuex";
 
-import modules from './modules';
+import modules from "./modules";
 
 Vue.use(Vuex);
 
-const vuexPersist = new VuexPersist({
-    key: 'my-app',
-    storage: window.localStorage
-});
-
 export default new Vuex.Store({
-    modules,
-    actions: {
-        reset({ commit }) {
-            Object.keys(modules).forEach(moduleName => {
-                commit(`${moduleName}/RESET`);
-            });
-        }
-    },
-    plugins: [
-        vuexPersist.plugin
-    ]
+  modules,
+  actions: {
+    reset({ commit }) {
+      Object.keys(modules).forEach(moduleName => {
+        commit(`${moduleName}/RESET`);
+      });
+    }
+  }
 });
