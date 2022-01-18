79cbec8a75a0b13a05086028b75e0cef4ddf4258
Michell Algarra <michell.algarra@gmail.com>
2022-01-18 14:21:05 -0300

    feat(Management.vue): added the subjects tab

--- src/components/Admin/Management.vue
+++ src/components/Admin/Management.vue
@@ -1,101 +1,125 @@
 <template>
-    <v-container>
-        <div class="text-center mt-5 mb-5">
-            <h1 class="blue--text">Administração</h1>
-        </div>
+  <v-container>
+    <div class="text-center mt-5 mb-5">
+      <h1 class="blue--text">Administração</h1>
+    </div>
 
-        <template>
-            <v-tabs
-                v-model='tab'
-                background-color="white" >
-                <v-tab>
-                    <v-icon left>
-                        {{ mdiDatabase }}
-                    </v-icon>
-                    Backups
-                </v-tab>
-                <v-tab>
-                    <v-icon left>
-                        {{ mdiAccount }}
-                    </v-icon>
-                    Usuários
-                </v-tab>
-                <v-tab>
-                    <v-icon left>
-                        {{ mdiFileDocument }}
-                    </v-icon>
-                    Logs
-                </v-tab>
-            </v-tabs>
+    <template>
+      <v-tabs v-model="tab" background-color="white">
+        <v-tab>
+          <v-icon left>
+            {{ mdiDatabase }}
+          </v-icon>
+          Backups
+        </v-tab>
+        <v-tab>
+          <v-icon left>
+            {{ mdiBallot }}
+          </v-icon>
+          Assuntos
+        </v-tab>
+        <v-tab>
+          <v-icon left>
+            {{ mdiAccount }}
+          </v-icon>
+          Usuários
+        </v-tab>
+        <v-tab>
+          <v-icon left>
+            {{ mdiFileDocument }}
+          </v-icon>
+          Logs
+        </v-tab>
+      </v-tabs>
 
-            <v-tabs-items v-model="tab" >
-                <v-tab-item>
-                    <v-card>
-                        <BackupsTable />
-                    </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                    <v-card>
-                        <UsersTable />
-                    </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                    <v-card>
-                        <LogsTable />
-                    </v-card>
-                </v-tab-item>
-            </v-tabs-items>
-        </template>
-    </v-container>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card>
+            <BackupsTable />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <Subjects />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <UsersTable />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <LogsTable />
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </template>
+  </v-container>
 </template>
 
 <script>
-    import { mdiFileDocument, mdiAccount, mdiDatabase } from '@mdi/js';
-    import BackupsTable from './BackupsTable';
-    import UsersTable from './UsersTable';
-    import LogsTable from './LogsTable';
+import { mdiFileDocument, mdiAccount, mdiDatabase, mdiBallot } from "@mdi/js";
+import BackupsTable from "./BackupsTable";
+import Subjects from "./Subjects";
+import UsersTable from "./UsersTable";
+import LogsTable from "./LogsTable";
 
-    export default {
-        name: 'Management',
-        components: { BackupsTable, UsersTable, LogsTable },
-        data() {
-            return {
-                mdiDatabase,
-                mdiAccount,
-                mdiFileDocument,
-                tab: null,
-                isBackupsLoaded: false,
-                isUsersLoaded: false,
-                isLogsLoaded: false
-            }
-        },
-        watch: {
-            tab(number) {
-                if (number === 0 && !this.isBackupsLoaded) {
-                    this.$store.dispatch('loadBackups');
-                    this.isBackupsLoaded = true;
-                } else if (number === 1 && !this.isUsersLoaded) {
-                    this.$store.dispatch('loadUsers');
-                    this.isUsersLoaded = true;
-                } else if (number === 2 && !this.isLogsLoaded) {
-                    this.$store.dispatch('loadLogs');
-                    this.isLogsLoaded = true;
-                }
-            }
-        },
-        created() {
-            const queryTab = this.$route.query.tab;
-            if (queryTab) {
-                if (queryTab === 'backups') {
-                    this.tab = 0;
-                } else if (queryTab === 'users') {
-                    this.tab = 1;
-                } else {
-                    this.tab = 2;
-                }
-            } else {
-                this.tab = 0;
-            }
-        }
+export default {
+  name: "Management",
+  components: { BackupsTable, Subjects, UsersTable, LogsTable },
+  data() {
+    return {
+      mdiDatabase,
+      mdiAccount,
+      mdiFileDocument,
+      mdiBallot,
+      tab: null,
+      isBackupsLoaded: false,
+      isSubjectsLoaded: false,
+      isUsersLoaded: false,
+      isLogsLoaded: false
+    };
+  },
+  watch: {
+    tab(number) {
+      if (number === 0 && !this.isBackupsLoaded) {
+        this.$store.dispatch("loadBackups");
+        this.isBackupsLoaded = true;
+      } else if (number === 1 && !this.isSubjectsLoaded) {
+        this.$store.dispatch("loadSubjects");
+        this.isSubjectsLoaded = true;
+      } else if (number === 2 && !this.isUsersLoaded) {
+        this.$store.dispatch("loadUsers");
+        this.isUsersLoaded = true;
+      } else if (number === 3 && !this.isLogsLoaded) {
+        this.$store.dispatch("loadLogs");
+        this.isLogsLoaded = true;
+      }
     }
+  },
+  created() {
+    const queryTab = this.$route.query.tab;
+    if (queryTab) {
+      switch (queryTab) {
+        case "backups":
+          this.tab = 0;
+          break;
+        case "subjects":
+          this.tab = 1;
+          break;
+        case "users":
+          this.tab = 2;
+          break;
+        case "logs":
+          this.tab = 3;
+          break;
+        default:
+          this.tab = 0;
+      }
+    } else {
+      this.tab = 0;
+    }
+  }
+};
 </script>
