cd5a0f03e78f79ce296b3c45c21c4eed6e8dac76
Michell Algarra <michell.algarra@gmail.com>
2022-02-07 16:41:59 -0300

    refactor(QuestionImage.vue): improved the image checking

--- src/components/Questions/QuestionImage.vue
+++ src/components/Questions/QuestionImage.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="img-container mb-5">
-        <v-container v-if="imageSize === '1x'">
-            <img :src="image" style="max-height: 40vh; max-width: 40vw" />
-        </v-container>
+  <div class="img-container mb-5">
+    <v-container v-if="imageSize === '1x'">
+      <img :src="image" style="max-height: 40vh; max-width: 40vw" />
+    </v-container>
 
-        <v-container v-else-if="imageSize === '2x'">
-            <img :src="image" style="max-height: 50vh; max-width: 50vw" />
-        </v-container>
+    <v-container v-else-if="imageSize === '2x'">
+      <img :src="image" style="max-height: 50vh; max-width: 50vw" />
+    </v-container>
 
-        <v-container v-else-if="imageSize === '3x'">
-            <img :src="image" style="max-height: 60vh; max-width: 60vw" />
-        </v-container>
+    <v-container v-else-if="imageSize === '3x'">
+      <img :src="image" style="max-height: 60vh; max-width: 60vw" />
+    </v-container>
 
-        <v-container v-if="imageSize === undefined">
-            <img :src="image" style="max-height: 50vh; max-width: 50vw" />
-        </v-container>
-    </div>
+    <v-container v-if="!imageSize">
+      <img :src="image" style="max-height: 50vh; max-width: 50vw" />
+    </v-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Image',
-        props: ['image', 'imageSize']
-    }
+export default {
+  name: "QuestionImage",
+  props: ["image", "imageSize"]
+};
 </script>
 
 <style>
-    @media print {
-        .img-container {
-            text-align: center !important;
-        }
-    }
+@media print {
+  .img-container {
+    text-align: center !important;
+  }
+}
 </style>
