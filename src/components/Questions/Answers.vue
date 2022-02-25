27381cf51f8e15a6992d5e627068afe2a9afe383
Michell Algarra <michell.algarra@gmail.com>
2022-02-25 16:49:15 -0300

    feat(Answers.vue): removed multi column answers

--- src/components/Questions/Answers.vue
+++ src/components/Questions/Answers.vue
@@ -1,61 +1,33 @@
 <template>
-    <div>
-        <div v-if="isMultiColumn">
-            <v-row class='answer-block'>
-                <v-col cols="auto">
-                    <div style="opacity: 0">A -</div>
-                </v-col>
-                <v-col v-for="(item, index) in answers[0].text" :key="index">
-                    <vue-markdown :source="item.title || ''" />
-                </v-col>
-            </v-row>
-
-            <v-row v-for="(ans, i) in answers" :key="i" class='answer-block'>
-                <v-col cols="auto">{{ letters[i] }}</v-col>
-                <v-col v-for="(item, index) in answers[i].text" :key="index">
-                    <vue-markdown :source="item.answerDescription || ''" />
-                </v-col>
-            </v-row>
-        </div>
-
-        <div v-else>
-            <v-row v-for="(item, index) in answers" :key="index" class='answer-block'>
-                <v-col cols="auto">{{ letters[index] }}</v-col>
-                <v-col>
-                    <vue-markdown :source="item.text || ''" />
-                </v-col>
-            </v-row>
-        </div>
-    </div>
+  <div>
+    <v-row
+      v-for="(item, index) in answers"
+      :key="index"
+      class="answer-block pa-0 ma-0"
+    >
+      <span>
+        <strong class="mr-1">{{ letters[index] }}</strong>
+        {{ item.text }}
+      </span>
+    </v-row>
+  </div>
 </template>
 
 <script>
-    import VueMarkdown from "vue-markdown";
-    import "vue-markdown";
-
-    export default {
-        name: 'Answers',
-        props: ['answers'],
-        components: { 'vue-markdown': VueMarkdown },
-        data() {
-            return {
-                letters: ["A -", "B -", "C -", "D -"]
-            }
-        },
-        computed: {
-            isMultiColumn() {
-                return this.answers && this.answers[0] && typeof(this.answers[0].text) !== 'string';
-            }
-        }
-    }
+export default {
+  name: "Answers",
+  props: ["answers"],
+  data() {
+    return {
+      letters: ["A. ", "B. ", "C. ", "D. "]
+    };
+  }
+};
 </script>
 
 <style>
-    .answer-block {
-        margin: 0 !important;
-    }
-
-    .answer-block .col {
-        padding: 0 10px 0 10px !important;
-    }
+.answer-block {
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
