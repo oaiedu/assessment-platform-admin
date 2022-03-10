61af3801aa39dbed74b9dff614cee79b8ae915ac
Michell Algarra <michell.algarra@gmail.com>
2022-03-10 11:20:01 -0300

    build: commented bundle analyzer plugin

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,28 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    assetsDir: 'assets/',
-    configureWebpack: {
-        plugins: [
-            new BundleAnalyzerPlugin()
-        ]
-    },
-    chainWebpack: config => {
-        config.plugin('VuetifyLoaderPlugin').tap(args => [{
-            match(originalTag, { kebabTag, camelTag, path, component }) {
-                if(kebabTag.startsWith('core-')) {
-                    return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
-                }
-            }
-        }])
-    },
-    'transpileDependencies': [
-        'vuetify'
-    ],
-    lintOnSave: false
-}
+  assetsDir: "assets/",
+  // configureWebpack: {
+  //     plugins: [
+  //         new BundleAnalyzerPlugin()
+  //     ]
+  // },
+  chainWebpack: config => {
+    config.plugin("VuetifyLoaderPlugin").tap(args => [
+      {
+        match(originalTag, { kebabTag, camelTag, path, component }) {
+          if (kebabTag.startsWith("core-")) {
+            return [
+              camelTag,
+              `import ${camelTag} from '@/components/core/${camelTag.substring(
+                4
+              )}.vue'`
+            ];
+          }
+        }
+      }
+    ]);
+  },
+  transpileDependencies: ["vuetify"],
+  lintOnSave: false
+};
