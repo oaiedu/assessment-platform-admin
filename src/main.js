b434d6f67bc5aed13ec282aa534e61b2343f39e0
Michell Algarra <michell.algarra@gmail.com>
2022-03-11 11:44:00 -0300

    feat(main.js): changed how firebase gets its config

--- src/main.js
+++ src/main.js
@@ -8,23 +8,11 @@ import "firebase/storage";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { getConfig } from "./api/firebase";
 
 Vue.config.productionTip = false;
 
-console.log(process.env);
-
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(getConfig());
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
