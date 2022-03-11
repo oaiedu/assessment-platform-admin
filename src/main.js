14caaa5e842ebf8d3ad7a92ed346a5273e31fd83
Michell Algarra <michell.algarra@gmail.com>
2022-03-11 11:18:37 -0300

    feat: added a console log to debug the ci deploy

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
+console.log(process.env);
+
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
