eb4f4c03f167b95311ca23437e247b2f8b8d3785
MuriloND <murilonog2013@gmail.com>
2022-04-04 13:41:52 -0300

    Theme Switch Button v1.0 implemented

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-switch
+        v-model="darkMode"
+        inset
+        :label="`Dark Mode`"
+    ></v-switch>
+</template>
+
+<script>
+    data() {
+        return {
+            darkMode: false
+        }
+    },
+    watch: {
+        darkMode(value) {
+            this.$store.commit('setTheme', value ? 'dark' : 'light')
+        }
+    };
+</script>
