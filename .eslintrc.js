3da094739e60d4db307b3a6bab513bf7df4b7b91
Michell Algarra <michell.algarra@gmail.com>
2023-01-26 15:21:41 -0300

    build: changed js version to 2021

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "plugin:vue/essential",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: "plugin:vue/essential",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module"
+  },
+  plugins: ["vue"],
+  rules: {}
 };
