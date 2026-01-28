17f04ec2809a673de3dd488d773c426ed227d87d
rahul810050 <rahul810050@gmail.com>
2026-01-28 22:54:12 +0530

    chore: move firebase config to environment variables
    Signed-off-by: rahul810050 <rahul810050@gmail.com>

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -3,26 +3,12 @@
  *
  * @returns An object that keeps all of the firebase configuration.
  */
-export const getConfig = () => {
-  const prodConfig = {
-    apiKey: "AIzaSyAXJ3DvXmuoxq4BREOl8pymt6t8NJnX6TA",
-    authDomain: "cloud-quiz-generator.firebaseapp.com",
-    projectId: "cloud-quiz-generator",
-    storageBucket: "cloud-quiz-generator.appspot.com",
-    messagingSenderId: "684103556095",
-    appId: "1:684103556095:web:30fcc211c80a15af86e5ca",
-    measurementId: "G-E4CRZVV3K9"
-  };
-
-  const devConfig = {
-    apiKey: "AIzaSyBBulAjoVwdI1CaK1GFCr1gh0F5JBniZoM",
-    authDomain: "develop-lg-quiz-generator.firebaseapp.com",
-    projectId: "develop-lg-quiz-generator",
-    storageBucket: "develop-lg-quiz-generator.appspot.com",
-    messagingSenderId: "149140593266",
-    appId: "1:149140593266:web:a16dec7371623437dac0eb",
-    measurementId: "G-18JVW1N909"
-  };
-
-  return process.env.NODE_ENV === "production" ? prodConfig : devConfig;
-};
+export const getConfig = ()=> ({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+})
